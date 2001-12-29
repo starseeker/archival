c71d092e2eb6c7d0fba2bcd8ff75d0c658606fad
Randolf Schultz <randolf.schultz@gmail.com>
2001-12-29 13:53:37 +0000

    changed path defaults to include old path via :&

--- ayam/src/tcl/root.tcl
+++ ayam/src/tcl/root.tcl
@@ -60,9 +60,9 @@ addParam $w RiOptData RadSteps
 addParam $w RiOptData PatchSamples
 
 addText $w e6 "Paths:"
-addString $w RiOptData Textures [list textures]
-addString $w RiOptData Includes [list includes]
-addString $w RiOptData Shaders [list shaders]
+addString $w RiOptData Textures [list "textures:&" "textures"]
+addString $w RiOptData Includes [list "includes:&" "includes"]
+addString $w RiOptData Shaders [list "shaders:&" "shaders"]
 
 addText $w e7 "Limits:"
 addParam $w RiOptData TextureMem
