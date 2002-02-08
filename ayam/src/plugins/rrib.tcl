31cf0e54baaa6ef166b4f78d71a8b07c327c0050
Randolf Schultz <randolf.schultz@gmail.com>
2002-02-08 15:22:20 +0000

    added copyright banner

--- ayam/src/plugins/rrib.tcl
+++ ayam/src/plugins/rrib.tcl
@@ -1,4 +1,15 @@
-# io_insertScene:
+# Ayam, a free 3D modeler for the RenderMan interface.
+#
+# Ayam is copyrighted 1998-2001 by Randolf Schultz
+# (rschultz@informatik.uni-rostock.de) and others.
+#
+# All rights reserved.
+#
+# See the file License for details.
+
+# rrib.tcl - RRIB plugin GUI code
+
+# rrib_import:
 #
 #
 proc rrib_import { } {
