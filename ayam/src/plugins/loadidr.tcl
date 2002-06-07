e10425f1c48d7f67600478c64fa89f1c10245ed7
Randolf Schultz <randolf.schultz@gmail.com>
2002-06-07 22:55:48 +0000

    improved loading

--- ayam/src/plugins/loadidr.tcl
+++ ayam/src/plugins/loadidr.tcl
@@ -1,4 +1,8 @@
-cd plugins
-catch {io_lc idr.so}
-cd ..
+set oldcd [pwd]
+cd [file dirname [info nameofexecutable]]/plugins
+catch {io_lc idr.so} result
+if { $result != "" } {
+    puts $result
+}
+cd $oldcd
 return;
--- ayam/src/plugins/loadrrib.tcl
+++ ayam/src/plugins/loadrrib.tcl
@@ -1,4 +1,8 @@
-cd plugins
-catch {io_lc rrib.so}
-cd ..
+set oldcd [pwd]
+cd [file dirname [info nameofexecutable]]/plugins
+catch {io_lc rrib.so} result
+if { $result != "" } {
+    puts $result
+}
+cd $oldcd
 return;
