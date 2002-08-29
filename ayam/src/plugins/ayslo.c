fcdcb804a96d1b0d10a805c6670f998f961606d7
Randolf Schultz <randolf.schultz@gmail.com>
2002-08-29 10:04:41 +0000

    initial revision for Ayam1.3

---
+++ ayam/src/plugins/ayslo.c
@@ -0,0 +1,276 @@
+/*
+ * Ayam, a free 3D modeler for the RenderMan interface.
+ *
+ * Ayam is copyrighted 1998-2001 by Randolf Schultz
+ * (rschultz@informatik.uni-rostock.de) and others.
+ *
+ * All rights reserved.
+ *
+ * See the file License for details.
+ *
+ */
+
+#include "ayam.h"
+
+#include <slo.h>
+
+/* ayslo.c - Plug-In to scan shaders compiled with shader (PRMan)
+   using libsloargs  */
+
+/* prototypes of functions local to this module */
+int ayslo_scanslosarg(SLO_VISSYMDEF *symbol, Tcl_DString *ds);
+
+int ayslo_scanslotcmd(ClientData clientData, Tcl_Interp *interp,
+			    int argc, char *argv[]);
+
+int Ayslo_Init(Tcl_Interp *interp);
+
+/* functions: */
+
+/* ayslo_scanslosarg:
+ *  helper for ayslo_scanslotcmd
+ *  scan a single shader argument
+ */
+int
+ayslo_scanslosarg(SLO_VISSYMDEF *symbol, Tcl_DString *ds)
+{
+ int ay_status = AY_OK;
+ char buffer[255];
+ double deffltval;
+ char *defstrval;
+ int j;
+
+  switch(symbol->svd_type)
+    {
+    case SLO_TYPE_POINT:
+    case SLO_TYPE_COLOR:
+    case SLO_TYPE_VECTOR:
+    case SLO_TYPE_NORMAL:
+      Tcl_DStringAppend(ds, "{ ", -1);
+      deffltval = (double)((symbol->svd_default).pointval->xval);
+      sprintf(buffer, "%g ", deffltval);
+      Tcl_DStringAppend(ds, buffer, -1);
+      deffltval = (double)((symbol->svd_default).pointval->yval);
+      sprintf(buffer, "%g ", deffltval);
+      Tcl_DStringAppend(ds, buffer, -1);
+      deffltval = (double)((symbol->svd_default).pointval->zval);
+      sprintf(buffer, "%g ", deffltval);
+      Tcl_DStringAppend(ds, buffer, -1);
+      Tcl_DStringAppend(ds, "} ", -1);
+      break;
+    case SLO_TYPE_MATRIX:
+      Tcl_DStringAppend(ds, "{ ", -1);
+      for(j = 0; j < 16; j++)
+	{
+	  deffltval = (double)((symbol->svd_default).matrixval[j]);
+	  sprintf(buffer, "%g ", deffltval);
+	  Tcl_DStringAppend(ds, buffer, -1);
+	} /* for */
+      Tcl_DStringAppend(ds, "} ", -1);
+      break;
+    case SLO_TYPE_SCALAR:
+      deffltval = (double)(*(symbol->svd_default).scalarval);
+      sprintf(buffer, "%g ", deffltval);
+      Tcl_DStringAppend(ds, buffer, -1);
+      break;
+    case SLO_TYPE_STRING:
+      defstrval = (symbol->svd_default).stringval;
+      Tcl_DStringAppend(ds, defstrval, -1);
+      Tcl_DStringAppend(ds, " ", -1);
+      break;
+    default:
+      break;
+    } /* switch */
+
+ return ay_status;
+} /* ayslo_scanslosarg */
+
+
+/* ayslo_scanslotcmd:
+ *  scan a shader compiled with shader using libsoargs
+ */
+int
+ayslo_scanslotcmd(ClientData clientData, Tcl_Interp *interp,
+			int argc, char *argv[])
+{
+ char fname[] = "shaderScanSLO";
+ int i = 0, j = 0, numargs = 0;
+ SLO_VISSYMDEF *symbol = NULL, *element = NULL;
+ SLO_TYPE type;
+ char buffer[255];
+ int arraylen;
+ Tcl_DString ds;
+ char vname[] = "ayprefs(Shaders)";
+
+  if(argc < 3)
+    {
+      ay_error(AY_EARGS, fname, "shaderpath varname");
+      return TCL_OK;
+    }
+
+  slo_SetPath(Tcl_GetVar(ay_interp, vname, TCL_GLOBAL_ONLY|TCL_LEAVE_ERR_MSG));
+
+  if((slo_SetShader(argv[1])) == -1)
+    {
+      ay_error(AY_ERROR, fname, "slo_SetShader failed for:");
+      ay_error(AY_ERROR, fname, argv[1]);
+      return TCL_OK;
+    }
+
+  Tcl_DStringInit(&ds);
+
+  /* get name of shader */
+  Tcl_DStringAppend(&ds, argv[1]/*slo_GetName()*/, -1);
+
+  /* get type of shader */
+  type = slo_GetType();
+  switch (type)
+    {
+    case SLO_TYPE_SURFACE:
+      Tcl_DStringAppend(&ds, " surface ", -1);
+     break;
+   case SLO_TYPE_DISPLACEMENT:
+      Tcl_DStringAppend(&ds, " displacement ", -1);
+     break;
+   case SLO_TYPE_LIGHT:
+      Tcl_DStringAppend(&ds, " light ", -1);
+     break;
+   case SLO_TYPE_VOLUME:
+     Tcl_DStringAppend(&ds, " volume ", -1);
+     break;
+   case SLO_TYPE_IMAGER:
+     Tcl_DStringAppend(&ds, " imager ", -1);
+     break;
+   case SLO_TYPE_TRANSFORMATION:
+     Tcl_DStringAppend(&ds, " transformation ", -1);
+     break;
+   default:
+     break;
+   }
+
+  /* get arguments of shader */
+  numargs = slo_GetNArgs();
+  Tcl_DStringAppend(&ds, "{ ", -1);
+  for(i = 1; i <= numargs; i++)
+    {
+
+      symbol = NULL;
+      symbol = slo_GetArgById(i);
+
+      if(!symbol)
+	{
+	  ay_error(AY_ERROR, fname, "Cannot get symbol from shader:");
+	  ay_error(AY_ERROR, fname, argv[1]);
+	  /*
+	  slo_EndShader();
+	  Tcl_DStringFree(&ds);
+	  return TCL_OK;
+	  */
+	}
+
+      /* XXXX temporarily discard array arguments   */
+      if(symbol->svd_arraylen < 1)
+	{
+
+      Tcl_DStringAppend(&ds, "{ ", -1);
+      Tcl_DStringAppend(&ds, symbol->svd_name, -1);
+      Tcl_DStringAppend(&ds, " ", -1 );
+
+      switch(symbol->svd_type)
+	{
+	case SLO_TYPE_POINT:
+	  Tcl_DStringAppend(&ds, "point ", -1);
+	  break;
+	case SLO_TYPE_COLOR:
+	  Tcl_DStringAppend(&ds, "color ", -1);
+	  break;
+	case SLO_TYPE_VECTOR:
+	  Tcl_DStringAppend(&ds, "vector ", -1);
+	  break;
+	case SLO_TYPE_NORMAL:
+	  Tcl_DStringAppend(&ds, "normal ", -1);
+	  break;
+	case SLO_TYPE_MATRIX:
+	  Tcl_DStringAppend(&ds, "matrix ", -1);
+	  break;
+	case SLO_TYPE_SCALAR:
+	  Tcl_DStringAppend(&ds, "float ", -1);
+	  break;
+	case SLO_TYPE_STRING:
+	  Tcl_DStringAppend(&ds, "string ", -1);
+	  break;
+	default:
+	  Tcl_DStringAppend(&ds, "unknown ", -1);
+	  break;
+	}
+
+      arraylen = symbol->svd_arraylen;
+      sprintf(buffer, "%d ", arraylen);
+      Tcl_DStringAppend(&ds, buffer, -1);
+
+      if(arraylen > 0)
+	{
+	  Tcl_DStringAppend(&ds, "{ ", -1);
+	  for(j = 0; j < arraylen; j++)
+	    {
+	      element = NULL;
+	      element = slo_GetArrayArgElement(symbol, j);
+	      if(!element)
+		{
+		  ay_error(AY_ERROR, fname, "Could not get array element:");
+		  ay_error(AY_ERROR, fname, symbol->svd_name);
+		  Tcl_DStringFree(&ds);
+		  return TCL_OK;
+		} /* if */
+	      ayslo_scanslosarg(element, &ds);
+	    } /* for */
+
+	  Tcl_DStringAppend(&ds, "} ", -1);
+	}
+      else
+	{
+	  ayslo_scanslosarg(symbol, &ds);
+	} /* if */
+      Tcl_DStringAppend(&ds, "} ", -1);
+
+	} 
+      else
+	{
+	  ay_error(AY_EWARN,fname,"Skipping array argument!");
+	  /*	  ay_error(AY_EWARN,fname,symbol->svd_name);*/
+	} /* if */
+      /* XXXX temporarily discard array arguments */
+    } /* for */
+  Tcl_DStringAppend(&ds, "} ", -1);
+
+
+  slo_EndShader();
+
+  Tcl_SetVar(interp, argv[2], Tcl_DStringValue(&ds), TCL_LEAVE_ERR_MSG);
+
+  Tcl_DStringFree(&ds);
+
+ return TCL_OK;
+} /* ayslo_scanslotcmd */
+
+
+/* note: this function _must_ be capitalized exactly this way
+ * regardless of filename (see: man n load)!
+ */
+int
+Ayslo_Init(Tcl_Interp *interp)
+{
+ char fname[] = "ayslo_init";
+
+  Tcl_SetVar(interp, "ay(sext)", ".slo", TCL_LEAVE_ERR_MSG | TCL_GLOBAL_ONLY);
+
+  Tcl_CreateCommand(interp, "shaderScan", ayslo_scanslotcmd,
+		    (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
+
+  ay_error(AY_EOUTPUT, fname,
+	   "Plug-In \\\"ayslo\\\" successfully loaded.");
+  ay_error(AY_EOUTPUT, fname,
+	   "Ayam will now scan for .slo-shaders only!");
+
+ return TCL_OK;
+} /* Ayslo_Init */
---
+++ ayam/src/plugins/ayso.c
@@ -0,0 +1,281 @@
+/*
+ * Ayam, a free 3D modeler for the RenderMan interface.
+ *
+ * Ayam is copyrighted 1998-2001 by Randolf Schultz
+ * (rschultz@informatik.uni-rostock.de) and others.
+ *
+ * All rights reserved.
+ *
+ * See the file License for details.
+ *
+ */
+
+/* force ayam.h to not include BMRT/slc.h as this would clash with RDC/so.h
+   due to a double defined SO_H */
+#ifdef AYUSESLCARGS
+#undef AYUSESLCARGS
+#endif
+
+#include "ayam.h"
+
+#include "so.h"
+
+/* ayso.c - Plug-In to scan shaders compiled with shaderdc (RenderDotC)
+   using libsoargs  */
+
+/* prototypes of functions local to this module */
+int ayso_scansosarg(SO_VISSYMDEF *symbol, Tcl_DString *ds);
+
+int ayso_scansotcmd(ClientData clientData, Tcl_Interp *interp,
+			    int argc, char *argv[]);
+
+int Ayso_Init(Tcl_Interp *interp);
+
+/* functions: */
+
+/* ayso_scansosarg:
+ *  helper for ayso_scansotcmd
+ *  scan a single shader argument
+ */
+int
+ayso_scansosarg(SO_VISSYMDEF *symbol, Tcl_DString *ds)
+{
+ int ay_status = AY_OK;
+ char buffer[255];
+ double deffltval;
+ char *defstrval;
+ int j;
+
+  switch(symbol->svd_type)
+    {
+    case SO_TYPE_POINT:
+    case SO_TYPE_COLOR:
+    case SO_TYPE_VECTOR:
+    case SO_TYPE_NORMAL:
+      Tcl_DStringAppend(ds, "{ ", -1);
+      deffltval = (double)((symbol->svd_default).pointval->xval);
+      sprintf(buffer, "%g ", deffltval);
+      Tcl_DStringAppend(ds, buffer, -1);
+      deffltval = (double)((symbol->svd_default).pointval->yval);
+      sprintf(buffer, "%g ", deffltval);
+      Tcl_DStringAppend(ds, buffer, -1);
+      deffltval = (double)((symbol->svd_default).pointval->zval);
+      sprintf(buffer, "%g ", deffltval);
+      Tcl_DStringAppend(ds, buffer, -1);
+      Tcl_DStringAppend(ds, "} ", -1);
+      break;
+    case SO_TYPE_MATRIX:
+      Tcl_DStringAppend(ds, "{ ", -1);
+      for(j = 0; j < 16; j++)
+	{
+	  deffltval = (double)((symbol->svd_default).matrixval[j]);
+	  sprintf(buffer, "%g ", deffltval);
+	  Tcl_DStringAppend(ds, buffer, -1);
+	} /* for */
+      Tcl_DStringAppend(ds, "} ", -1);
+      break;
+    case SO_TYPE_SCALAR:
+      deffltval = (double)(*(symbol->svd_default).scalarval);
+      sprintf(buffer, "%g ", deffltval);
+      Tcl_DStringAppend(ds, buffer, -1);
+      break;
+    case SO_TYPE_STRING:
+      defstrval = (symbol->svd_default).stringval;
+      Tcl_DStringAppend(ds, defstrval, -1);
+      Tcl_DStringAppend(ds, " ", -1);
+      break;
+    default:
+      break;
+    } /* switch */
+
+ return ay_status;
+} /* ayso_scansosarg */
+
+
+/* ayso_scansotcmd:
+ *  scan a shader compiled with shaderdc using libsoargs
+ */
+int
+ayso_scansotcmd(ClientData clientData, Tcl_Interp *interp,
+			int argc, char *argv[])
+{
+ char fname[] = "shaderScanSO";
+ int i = 0, j = 0, numargs = 0;
+ SO_VISSYMDEF *symbol = NULL, *element = NULL;
+ SO_TYPE type;
+ char buffer[255];
+ int arraylen;
+ Tcl_DString ds;
+ char vname[] = "ayprefs(Shaders)";
+
+  if(argc < 3)
+    {
+      ay_error(AY_EARGS, fname, "shaderpath varname");
+      return TCL_OK;
+    }
+
+  So_SetPath(Tcl_GetVar(ay_interp, vname, TCL_GLOBAL_ONLY|TCL_LEAVE_ERR_MSG));
+
+  if((So_SetShader(argv[1])) == -1)
+    {
+      ay_error(AY_ERROR, fname, "So_SetShader failed for:");
+      ay_error(AY_ERROR, fname, argv[1]);
+      return TCL_OK;
+    }
+
+  Tcl_DStringInit(&ds);
+
+  /* get name of shader */
+  Tcl_DStringAppend(&ds, argv[1]/*So_GetName()*/, -1);
+
+  /* get type of shader */
+  type = So_GetType();
+  switch (type)
+    {
+    case SO_TYPE_SURFACE:
+      Tcl_DStringAppend(&ds, " surface ", -1);
+     break;
+   case SO_TYPE_DISPLACEMENT:
+      Tcl_DStringAppend(&ds, " displacement ", -1);
+     break;
+   case SO_TYPE_LIGHT:
+      Tcl_DStringAppend(&ds, " light ", -1);
+     break;
+   case SO_TYPE_VOLUME:
+     Tcl_DStringAppend(&ds, " volume ", -1);
+     break;
+   case SO_TYPE_IMAGER:
+     Tcl_DStringAppend(&ds, " imager ", -1);
+     break;
+   case SO_TYPE_TRANSFORMATION:
+     Tcl_DStringAppend(&ds, " transformation ", -1);
+     break;
+   default:
+     break;
+   }
+
+  /* get arguments of shader */
+  numargs = So_GetNArgs();
+  Tcl_DStringAppend(&ds, "{ ", -1);
+  for(i = 1; i <= numargs; i++)
+    {
+
+      symbol = NULL;
+      symbol = So_GetArgById(i);
+
+      if(!symbol)
+	{
+	  ay_error(AY_ERROR, fname, "Cannot get symbol from shader:");
+	  ay_error(AY_ERROR, fname, argv[1]);
+	  
+	  So_EndShader();
+	  Tcl_DStringFree(&ds);
+	  return TCL_OK;	  
+	}
+
+      /* XXXX temporarily discard array arguments   */
+      if(symbol->svd_arraylen < 1)
+	{
+
+      Tcl_DStringAppend(&ds, "{ ", -1);
+      Tcl_DStringAppend(&ds, symbol->svd_name, -1);
+      Tcl_DStringAppend(&ds, " ", -1 );
+
+      switch(symbol->svd_type)
+	{
+	case SO_TYPE_POINT:
+	  Tcl_DStringAppend(&ds, "point ", -1);
+	  break;
+	case SO_TYPE_COLOR:
+	  Tcl_DStringAppend(&ds, "color ", -1);
+	  break;
+	case SO_TYPE_VECTOR:
+	  Tcl_DStringAppend(&ds, "vector ", -1);
+	  break;
+	case SO_TYPE_NORMAL:
+	  Tcl_DStringAppend(&ds, "normal ", -1);
+	  break;
+	case SO_TYPE_MATRIX:
+	  Tcl_DStringAppend(&ds, "matrix ", -1);
+	  break;
+	case SO_TYPE_SCALAR:
+	  Tcl_DStringAppend(&ds, "float ", -1);
+	  break;
+	case SO_TYPE_STRING:
+	  Tcl_DStringAppend(&ds, "string ", -1);
+	  break;
+	default:
+	  Tcl_DStringAppend(&ds, "unknown ", -1);
+	  break;
+	}
+
+      arraylen = symbol->svd_arraylen;
+      sprintf(buffer, "%d ", arraylen);
+      Tcl_DStringAppend(&ds, buffer, -1);
+
+      if(arraylen > 0)
+	{
+	  Tcl_DStringAppend(&ds, "{ ", -1);
+	  for(j = 0; j < arraylen; j++)
+	    {
+	      element = NULL;
+	      element = So_GetArrayArgElement(symbol, j);
+	      if(!element)
+		{
+		  ay_error(AY_ERROR, fname, "Could not get array element:");
+		  ay_error(AY_ERROR, fname, symbol->svd_name);
+		  Tcl_DStringFree(&ds);
+		  return TCL_OK;
+		} /* if */
+	      ayso_scansosarg(element, &ds);
+	    } /* for */
+
+	  Tcl_DStringAppend(&ds, "} ", -1);
+	}
+      else
+	{
+	  ayso_scansosarg(symbol, &ds);
+	} /* if */
+      Tcl_DStringAppend(&ds, "} ", -1);
+
+	} 
+      else
+	{
+	  ay_error(AY_EWARN,fname,"Skipping array argument!");
+	  /*	  ay_error(AY_EWARN,fname,symbol->svd_name);*/
+	} /* if */
+      /* XXXX temporarily discard array arguments */
+    } /* for */
+  Tcl_DStringAppend(&ds, "} ", -1);
+
+
+  So_EndShader();
+
+  Tcl_SetVar(interp, argv[2], Tcl_DStringValue(&ds), TCL_LEAVE_ERR_MSG);
+
+  Tcl_DStringFree(&ds);
+
+ return TCL_OK;
+} /* ayso_scansotcmd */
+
+
+/* note: this function _must_ be capitalized exactly this way
+ * regardless of filename (see: man n load)!
+ */
+int
+Ayso_Init(Tcl_Interp *interp)
+{
+ char fname[] = "ayso_init";
+
+  Tcl_SetVar(interp, "ay(sext)", ".so", TCL_LEAVE_ERR_MSG | TCL_GLOBAL_ONLY);
+
+  Tcl_CreateCommand(interp, "shaderScan", ayso_scansotcmd,
+		    (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
+
+  ay_error(AY_EOUTPUT, fname,
+	   "Plug-In \\\"ayso\\\" successfully loaded.");
+  ay_error(AY_EOUTPUT, fname,
+	   "Ayam will now scan for .so-shaders only!");
+
+ return TCL_OK;
+} /* Ayso_Init */
