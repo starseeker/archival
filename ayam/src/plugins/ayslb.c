eaf04a4630a015e1fc7fb5c1dc0970aee026c888
Randolf Schultz <randolf.schultz@gmail.com>
2002-09-02 13:02:49 +0000

    improved comments

--- ayam/src/plugins/ayslb.c
+++ ayam/src/plugins/ayslb.c
@@ -16,14 +16,14 @@
    using code kindly provided by Scott Iverson (SiTex Graphics) */
 
 /*
-
-   Routines for obtaining the parameters for
-   shaders compiled to .slb files for AIR
-
-   (c) 2000 SiTex Graphics.  All rights reserved.
-
-   Last Modified:  July 14, 2000
-
+ 
+    Routines for obtaining the parameters for
+    shaders compiled to .slb files for AIR
+ 
+    (c) 2000 SiTex Graphics.  All rights reserved.
+ 
+    Last Modified:  July 14, 2000
+ 
 */
 
 /* prototypes of functions local to this module: */
@@ -91,9 +91,9 @@ typedef struct {
 } MagicHeader;
 
 /* functions: */
-
 /* ayslb_scanshader: Returns a fully qualified shader
-   declaration in the Tcl_DString pointed to by ds
+ *  declaration of the shader in file "fname" in the Tcl_DString
+ *  pointed to by "ds"
  */
 int
 ayslb_scanshader(char *fname, Tcl_DString *ds)
@@ -106,200 +106,205 @@ ayslb_scanshader(char *fname, Tcl_DString *ds)
  float *fp;
  FILE *f;
 
- f = fopen(fname,"rb");
- if (f)
-   {
-     t = (char *)&h1;
-     if (fread(t, 1, 4, f) == 4)
-       {
-	 t = (char *)&h;
-	 jj = h1.size;
-	 if (jj > sizeof(h))
-	   {
-	     jj = sizeof(h);
-	   }
+  f = fopen(fname,"rb");
+  if(f)
+    {
+      t = (char *)&h1;
+      if(fread(t, 1, 4, f) == 4)
+	{
+	  t = (char *)&h;
+	  jj = h1.size;
+	  if(jj > sizeof(h))
+	    {
+	      jj = sizeof(h);
+	    }
 
-	 if (fread(t, 1, sizeof(h), f) > 0)
-	   {
-	     switch (h.type)
-	       {
-	       case shSurface:
-		 Tcl_DStringAppend(ds, " surface ", -1);
-		 break;
-	       case shDisplace:
-		 Tcl_DStringAppend(ds, " displacement ", -1);
-		 break;
-	       case shVolume:
-		 Tcl_DStringAppend(ds, " volume ", -1);
-		 break;
-	       case shImager:
-		 Tcl_DStringAppend(ds, " imager ", -1);
-		 break;
-	       case shLight:
-		 Tcl_DStringAppend(ds, " light ", -1);
-		 break;
-	       case shTransform:
-		 Tcl_DStringAppend(ds, " transformation ", -1);
-		 break;
-	       case shProcedure:
-		 ay_error(AY_EWARN, fname,
-			  "skipping unsupported procedure shader");
-		 return AY_ERROR; /* early exit! */
-		 /*
-		 Tcl_DStringAppend(ds, " procedure ", -1);
-		 */
-		 break;
-	       default:
-		 break;
-	       };
+	  if(fread(t, 1, sizeof(h), f) > 0)
+	    {
+	      switch(h.type)
+		{
+		case shSurface:
+		  Tcl_DStringAppend(ds, " surface ", -1);
+		  break;
+		case shDisplace:
+		  Tcl_DStringAppend(ds, " displacement ", -1);
+		  break;
+		case shVolume:
+		  Tcl_DStringAppend(ds, " volume ", -1);
+		  break;
+		case shImager:
+		  Tcl_DStringAppend(ds, " imager ", -1);
+		  break;
+		case shLight:
+		  Tcl_DStringAppend(ds, " light ", -1);
+		  break;
+		case shTransform:
+		  Tcl_DStringAppend(ds, " transformation ", -1);
+		  break;
+		case shProcedure:
+		  ay_error(AY_EWARN, fname,
+			   "skipping unsupported procedure shader");
+		  return AY_ERROR; /* early exit! */
+		  /*Tcl_DStringAppend(ds, " procedure ", -1);*/
+		  break;
+		default:
+		  ay_error(AY_EWARN, fname,
+			   "skipping shader of unkown type");
+		  return AY_ERROR; /* early exit! */
+		  break;
+		}
 
-	     /*
-	       t=fname;
-	       while (*fname!='\0') {
-	       if (*fname=='\\') {t=fname; t++;}
-	       fname++;
-	       };
-	       fname=strchr(t,'.');
-	       if (fname!=NULL) {
-	       ch=*fname; *fname='\0';
-	       strcat(s,t); *fname=ch;
-	       } else
-	       strcat(s,t);
-	     */
+	      /*
+		t=fname;
+		while (*fname!='\0') {
+		if (*fname=='\\') {t=fname; t++;}
+		fname++;
+		}
+		fname=strchr(t,'.');
+		if (fname!=NULL) {
+		ch=*fname; *fname='\0';
+		strcat(s,t); *fname=ch;
+		} else
+		strcat(s,t);
+	      */
 
-	     Tcl_DStringAppend(ds, "{ ", -1);
+	      Tcl_DStringAppend(ds, "{ ", -1);
 
-	     for(i = 0; i < h.nparm; i++)
-	       {
+	      for(i = 0; i < h.nparm; i++)
+		{
 
-		 /*  j=i/8;
-		     if ((h.vary[j] && (1>>((i-j*8)))))
-		     strcat(s,"varying "); */
-		 /*
-		   j=h.parm[i].pflags;
-		   if ((j==2) || (j==3)) strcat(s,"output ");
-		   if ((j==1) || (j==3)) strcat(s,"varying ");
-		 */
+		  /*  j=i/8;
+		      if ((h.vary[j] && (1>>((i-j*8)))))
+		      strcat(s,"varying "); */
+		  /*
+		    j=h.parm[i].pflags;
+		    if ((j==2) || (j==3)) strcat(s,"output ");
+		    if ((j==1) || (j==3)) strcat(s,"varying ");
+		  */
 
-		 na = h.parm[i].pnarr;
-		 if(na <= 1)
-		   {
-		     Tcl_DStringAppend(ds, "{ ", -1);
+		  na = h.parm[i].pnarr;
+		  /* XXXX temporarily skip all array parameters */
+		  if(na <= 1)
+		    {
+		      Tcl_DStringAppend(ds, "{ ", -1);
 
-		     Tcl_DStringAppend(ds, h.parm[i].pname, -1);
+		      Tcl_DStringAppend(ds, h.parm[i].pname, -1);
 
-		     switch (h.parm[i].ptype)
-		       {
-		       case pString:
-			 Tcl_DStringAppend(ds, " string ", -1);
-			 break;
-		       case pFloat:
-			 Tcl_DStringAppend(ds, " float ", -1);
-			 break;
-		       case pColor:
-			 Tcl_DStringAppend(ds, " color ", -1);
-			 break;
-		       case pPoint:
-			 Tcl_DStringAppend(ds, " point ", -1);
-			 break;
-		       case pVector:
-			 Tcl_DStringAppend(ds, " vector ", -1);
-			 break;
-		       case pNormal:
-			 Tcl_DStringAppend(ds, " normal ", -1);
-			 break;
-		       case pMatrix:
-			 Tcl_DStringAppend(ds, " matrix ", -1);
-			 break;
-		       default:
-			 Tcl_DStringAppend(ds, " unknown ", -1);
-			 break;
-		       };
+		      switch(h.parm[i].ptype)
+			{
+			case pString:
+			  Tcl_DStringAppend(ds, " string ", -1);
+			  break;
+			case pFloat:
+			  Tcl_DStringAppend(ds, " float ", -1);
+			  break;
+			case pColor:
+			  Tcl_DStringAppend(ds, " color ", -1);
+			  break;
+			case pPoint:
+			  Tcl_DStringAppend(ds, " point ", -1);
+			  break;
+			case pVector:
+			  Tcl_DStringAppend(ds, " vector ", -1);
+			  break;
+			case pNormal:
+			  Tcl_DStringAppend(ds, " normal ", -1);
+			  break;
+			case pMatrix:
+			  Tcl_DStringAppend(ds, " matrix ", -1);
+			  break;
+			default:
+			  Tcl_DStringAppend(ds, " unknown ", -1);
+			  break;
+			}
 
-		     sprintf(buffer, "%d ", na);
-		     Tcl_DStringAppend(ds, buffer, -1);
+		      sprintf(buffer, "%d ", na);
+		      Tcl_DStringAppend(ds, buffer, -1);
 
-		     /* get default values */
-		     t = (char *)(&h);
-		     t += (sizeof(h) - (MAXNSHADERARGS-h.nparm) *
-		       sizeof(Parameter) - 128000 + h.ncode * 4);
-		     /*t += (h.dataix+h.parm[i].paddr*4-200);*/
-		     t += (h.parm[i].paddr * 4 - 200);
-		     fp = (float *)t;
+		      /* get default values */
+		      t = (char *)(&h);
+		      t += (sizeof(h) - (MAXNSHADERARGS-h.nparm) *
+			    sizeof(Parameter) - 128000 + h.ncode * 4);
+		      /*t += (h.dataix+h.parm[i].paddr*4-200);*/
+		      t += (h.parm[i].paddr * 4 - 200);
+		      fp = (float *)t;
 
-		     if(na > 1)
-		       {
-			 Tcl_DStringAppend(ds, "{ ", -1);
-		       }
+		      if(na > 1)
+			{
+			  Tcl_DStringAppend(ds, "{ ", -1);
+			}
 
-		     for (j = 0; j < na; j++)
-		       {
-			 switch (h.parm[i].ptype)
-			   {
-			   case pString:
-			     Tcl_DStringAppend(ds, t, -1);
-			     Tcl_DStringAppend(ds, " ", -1);
-			     t += 160;
-			     break;
-			   case pFloat:
-			     sprintf(buffer, "%f ", *fp); fp++;
-			     Tcl_DStringAppend(ds, buffer, -1);
-			     Tcl_DStringAppend(ds, " ", -1);
-			     break;
-			   case pColor:
-			   case pPoint:
-			   case pVector:
-			   case pNormal:
-			     Tcl_DStringAppend(ds, "{ ", -1);
-			     sprintf(buffer, "%f ", *fp); fp++;
-			     Tcl_DStringAppend(ds, buffer, -1);
-			     sprintf(buffer, "%f ", *fp); fp++;
-			     Tcl_DStringAppend(ds, buffer, -1);
-			     sprintf(buffer, "%f ", *fp); fp++;
-			     Tcl_DStringAppend(ds, buffer, -1);
-			     Tcl_DStringAppend(ds, "} ", -1);
-			     break;
-			   case pMatrix:
-			     Tcl_DStringAppend(ds, "{ ", -1);
-			     for (k = 1; k < 16; k++)
-			       {
-				 sprintf(buffer, "%f ", *fp); fp++;
-				 Tcl_DStringAppend(ds, buffer, -1);
-			       } /* for */
-			     Tcl_DStringAppend(ds, "} ", -1);
-			     break;
-			   default:
-			     break;
-			   } /* switch */
+		      for(j = 0; j < na; j++)
+			{
+			  switch (h.parm[i].ptype)
+			    {
+			    case pString:
+			      Tcl_DStringAppend(ds, t, -1);
+			      Tcl_DStringAppend(ds, " ", -1);
+			      t += 160;
+			      break;
+			    case pFloat:
+			      sprintf(buffer, "%f ", *fp); fp++;
+			      Tcl_DStringAppend(ds, buffer, -1);
+			      Tcl_DStringAppend(ds, " ", -1);
+			      break;
+			    case pColor:
+			    case pPoint:
+			    case pVector:
+			    case pNormal:
+			      Tcl_DStringAppend(ds, "{ ", -1);
+			      sprintf(buffer, "%f ", *fp); fp++;
+			      Tcl_DStringAppend(ds, buffer, -1);
+			      sprintf(buffer, "%f ", *fp); fp++;
+			      Tcl_DStringAppend(ds, buffer, -1);
+			      sprintf(buffer, "%f ", *fp); fp++;
+			      Tcl_DStringAppend(ds, buffer, -1);
+			      Tcl_DStringAppend(ds, "} ", -1);
+			      break;
+			    case pMatrix:
+			      Tcl_DStringAppend(ds, "{ ", -1);
+			      for(k = 1; k < 16; k++)
+				{
+				  sprintf(buffer, "%f ", *fp); fp++;
+				  Tcl_DStringAppend(ds, buffer, -1);
+				} /* for */
+			      Tcl_DStringAppend(ds, "} ", -1);
+			      break;
+			    default:
+			      break;
+			    } /* switch */
 
-		       } /* for */
-		     if(na > 1)
-		       {
-			 Tcl_DStringAppend(ds, "} ", -1);
-		       }
-		     Tcl_DStringAppend(ds, "} ", -1);
-		   } else {
-		     ay_error(AY_EWARN, h.parm[i].pname,
-			      "skipping array parameter");
-		     /* reset ay_errno so that scanAllShaders does not
-			stop parsing because of this */
-		     ay_error(AY_OK, NULL, NULL);
-		   } /* if */
-	       } /* for */
-	     Tcl_DStringAppend(ds, "}", -1);
-	   } /* if */
-       } /* if */
-     fclose(f);
+			} /* for */
+		      if(na > 1)
+			{
+			  Tcl_DStringAppend(ds, "} ", -1);
+			}
+		      Tcl_DStringAppend(ds, "} ", -1);
+		    } 
+		  else
+		    {
+		      ay_error(AY_EWARN, h.parm[i].pname,
+			       "skipping array parameter");
+		      /* reset ay_errno so that scanAllShaders does not
+			 stop parsing because of this */
+		      ay_error(AY_OK, NULL, NULL);
+		    } /* if */
+		} /* for */
+	      Tcl_DStringAppend(ds, "}", -1);
+	    } /* if */
+	} /* if */
 
-     return AY_OK;
-   } /* if */
+      fclose(f);
+
+      return AY_OK; /* early exit! */
+    } /* if */
 
  return AY_ERROR;
 } /* ayslb_scanshader */
 
 
 /* ayslb_scanslbtcmd:
- *  scan a shader compiled with shaded
+ *  scan a shader compiled with shaded (Air)
  */
 int
 ayslb_scanslbtcmd(ClientData clientData, Tcl_Interp *interp,
