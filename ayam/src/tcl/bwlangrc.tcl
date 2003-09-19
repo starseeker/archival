b225cb9a7f6bae97ab366cf57fd2cfb924e2abe2
Randolf Schultz <randolf.schultz@gmail.com>
2003-09-19 06:54:56 +0000

    initial revision for Ayam1.6

---
+++ ayam/src/tcl/bwlangrc.tcl
@@ -0,0 +1,36 @@
+# Ayam, a free 3D modeler for the RenderMan interface.
+#
+# Ayam is copyrighted 1998-2003 by Randolf Schultz
+# (rschultz@informatik.uni-rostock.de) and others.
+#
+# All rights reserved.
+#
+# See the file License for details.
+
+# bwlangrc.tcl - BWidgets1.2.1/lang/en.rc resources translated into option
+# calls, so that we may keep BWidgets in the wrapped binary
+
+option add *abortName Abort widgetDefault
+option add *retryName Retry widgetDefault
+option add *ignoreName Ignore widgetDefault
+option add *okName Ok widgetDefault
+option add *cancelName Cancel widgetDefault
+option add *yesName Yes widgetDefault
+option add *noName No widgetDefault
+option add *boldName Bold widgetDefault
+option add *italicName Italic widgetDefault
+option add *underlineName Underline widgetDefault
+option add *overstrikeName Overstrike widgetDefault
+option add *fontName Font widgetDefault
+option add *sizeName Size widgetDefault
+option add *styleName Style widgetDefault
+option add *loginName Login widgetDefault
+option add *passwordName Password widgetDefault
+option add *SelectFont.title "Font selection" widgetDefault
+option add *SelectFont.sampletext "Sample text" widgetDefault
+option add *MessageDlg.noneTitle Message widgetDefault
+option add *MessageDlg.infoTitle Information widgetDefault
+option add *MessageDlg.questionTitle Question widgetDefault
+option add *MessageDlg.warningTitle Warning widgetDefault
+option add *MessageDlg.errorTitle Error widgetDefault
+option add *PasswdDlg.title "Enter login and password" widgetDefault
