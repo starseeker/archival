59af39021ee35a3c903436a3e130e64e7e3ecb3a
Randolf Schultz <randolf.schultz@gmail.com>
2003-11-13 22:18:49 +0000

    renamed entries in Adaptive menu

--- ayam/src/contrib/meta/metaobj.tcl
+++ ayam/src/contrib/meta/metaobj.tcl
@@ -14,60 +14,41 @@ global ay MetaObj_props MetaObjAttr MetaObjAttrData
 
 set MetaObj_props { Transformations Attributes Material Tags MetaObjAttr }
 
-if {0} {
-proc stdReleaseBind { w } {
-     bind $w <ButtonRelease-1> {
-
-	if { $ayprefs(LazyNotify) == 1 } { forceNot }
-	rV
-	plb_update
-     
-      set meta_mouseup 1
-	 forceNot
-      update
-	 rV	 
-     }
-}
-# stdReleaseBind
-}
-
 proc metaobj_getAttr { } {
-global ay MetaObjAttr MetaObjAttrData
+    global ay MetaObjAttr MetaObjAttrData
 
-catch {destroy $ay(pca).$MetaObjAttr(w)}
-set w [frame $ay(pca).$MetaObjAttr(w)]
-getProp
+    catch {destroy $ay(pca).$MetaObjAttr(w)}
+    set w [frame $ay(pca).$MetaObjAttr(w)]
+    getProp
 
+    addParam $w MetaObjAttrData NumSamples {20 40 60 80 120 140 160 180 200}
+    addParam $w MetaObjAttrData IsoLevel 0.6
+    addCheck $w MetaObjAttrData "ShowWorld"
+    #addCheck $w MetaObjAttrData Adaptive
+    addMenu $w MetaObjAttrData Adaptive {Never Always Automatic}
 
-addParam $w MetaObjAttrData NumSamples {20 40 60 80 120 140 160 180 200}
-addParam $w MetaObjAttrData IsoLevel 0.6
-addCheck $w MetaObjAttrData "ShowWorld"
-#addCheck $w MetaObjAttrData Adaptive
-addMenu $w MetaObjAttrData Adaptive {none always automatic}
+    if {$MetaObjAttrData(Adaptive) != 0} {
+	addParam $w MetaObjAttrData Flatness 0.9
+	addParam $w MetaObjAttrData Epsilon 0.0001
+	addParam $w MetaObjAttrData StepSize 0.0001
+    }
 
-if {$MetaObjAttrData(Adaptive) != 0} {
-addParam $w MetaObjAttrData Flatness 0.9
-addParam $w MetaObjAttrData Epsilon 0.0001
-addParam $w MetaObjAttrData StepSize 0.0001
+    $ay(pca) itemconfigure 1 -window $w
+    plb_resize
+
 }
-
-$ay(pca) itemconfigure 1 -window $w
-plb_resize
-
-}
-
+# metaobj_getAttr
 
 array set MetaObjAttr {
-arr   MetaObjAttrData
-sproc ""
-gproc metaobj_getAttr
-w     fMetaObjAttr
-
+    arr   MetaObjAttrData
+    sproc ""
+    gproc metaobj_getAttr
+    w     fMetaObjAttr
 }
 
 array set MetaObjAttrData {
-Formula  0
-
+    Formula  0
+    Adaptive 0
 }
 
 # create MetaObjAttr-UI
@@ -76,17 +57,21 @@ set w [frame $ay(pca).$MetaObjAttr(w)]
 addParam $w MetaObjAttrData NumSamples {20 40 60 80 120 140 160 180 200}
 addParam $w MetaObjAttrData IsoLevel 0.6
 addCheck $w MetaObjAttrData "ShowWorld"
-addCheck $w MetaObjAttrData Adaptiv
+#addCheck $w MetaObjAttrData Adaptive
+addMenu $w MetaObjAttrData Adaptive {Never Always Automatic}
 
-##if {$MetaObjAttrData(Adaptiv) == 1} {
+##if {$MetaObjAttrData(Adaptive) == 1} {
 #addParam $w MetaObjAttrData Flatness 0.9
 #addParam $w MetaObjAttrData Epsilon 0.0001
 #addParam $w MetaObjAttrData Step 0.0001
 #}
-global meta_mouseup; set meta_mouseup 1
+
+global meta_mouseup
+set meta_mouseup 1
 
 # add menu entry to Create/Custom sub-menu
 mmenu_addcustom MetaObj "crtOb MetaObj;uS;sL;rV"
 
 # tell the rest of Ayam (or other custom objects), that we are loaded
 lappend ay(co) MetaObj
+
