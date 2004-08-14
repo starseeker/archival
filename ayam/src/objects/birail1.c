0eb18bd9b6704062eb917f095fe0a77447bf7c58
Randolf Schultz <randolf.schultz@gmail.com>
2004-08-14 10:34:12 +0000

    initial revision for Ayam 1.8

---
+++ ayam/src/objects/birail1.c
@@ -0,0 +1,731 @@
+/*
+ * Ayam, a free 3D modeler for the RenderMan interface.
+ *
+ * Ayam is copyrighted 1998-2004 by Randolf Schultz
+ * (rschultz@informatik.uni-rostock.de) and others.
+ *
+ * All rights reserved.
+ *
+ * See the file License for details.
+ *
+ */
+
+#include "ayam.h"
+
+/* birail1.c - birail1 object */
+
+static char *ay_birail1_name = "Birail1";
+
+int
+ay_birail1_createcb(int argc, char *argv[], ay_object *o)
+{
+ char fname[] = "crtbirail1";
+ ay_birail1_object *new = NULL;
+
+  if(!o)
+    return AY_ENULL;
+
+  if(!(new = calloc(1, sizeof(ay_birail1_object))))
+    {
+      ay_error(AY_EOMEM, fname, NULL);
+      return AY_ERROR;
+    }
+
+  new->rotate = AY_TRUE;
+  new->sections = 10;
+
+  o->parent = AY_TRUE;
+  o->refine = new;
+
+ return AY_OK;
+} /* ay_birail1_createcb */
+
+
+int
+ay_birail1_deletecb(void *c)
+{
+ ay_birail1_object *birail1 = NULL;
+
+  if(!c)
+    return AY_ENULL;    
+
+  birail1 = (ay_birail1_object *)(c);
+
+  if(birail1->npatch)
+    ay_object_delete(birail1->npatch);
+
+  if(birail1->start_cap)
+    ay_object_delete(birail1->start_cap);
+
+  if(birail1->end_cap)
+    ay_object_delete(birail1->end_cap);
+
+  free(birail1);
+
+ return AY_OK;
+} /* ay_birail1_deletecb */
+
+
+int
+ay_birail1_copycb(void *src, void **dst)
+{
+ ay_birail1_object *birail1 = NULL, *birail1src = NULL;
+
+  if(!src || !dst)
+    return AY_ENULL;
+
+  birail1src = (ay_birail1_object *)src;
+
+  if(!(birail1 = calloc(1, sizeof(ay_birail1_object))))
+    return AY_EOMEM; 
+
+  memcpy(birail1, src, sizeof(ay_birail1_object)); 
+
+
+  /* copy npatch */
+  ay_object_copy(birail1src->npatch, &(birail1->npatch));
+
+  /* copy start cap */
+  ay_object_copy(birail1src->start_cap, &(birail1->start_cap));
+
+  /* copy end cap */
+  ay_object_copy(birail1src->end_cap, &(birail1->end_cap));
+
+
+  *dst = (void *)birail1;
+
+ return AY_OK;
+} /* ay_birail1_copycb */
+
+
+int
+ay_birail1_drawcb(struct Togl *togl, ay_object *o)
+{
+ ay_birail1_object *birail1 = NULL;
+
+  if(!o)
+    return AY_ENULL;
+
+  birail1 = (ay_birail1_object *)o->refine;
+
+  if(!birail1)
+    return AY_ENULL;
+
+  if(birail1->npatch)
+    ay_draw_object(togl, birail1->npatch, AY_TRUE);
+
+  if(birail1->start_cap)
+    ay_draw_object(togl, birail1->start_cap, AY_TRUE);
+
+  if(birail1->end_cap)
+    ay_draw_object(togl, birail1->end_cap, AY_TRUE);
+
+ return AY_OK;
+} /* ay_birail1_drawcb */
+
+
+int
+ay_birail1_shadecb(struct Togl *togl, ay_object *o)
+{
+ ay_birail1_object *birail1 = NULL;
+
+  if(!o)
+    return AY_ENULL;
+
+  birail1 = (ay_birail1_object *)o->refine;
+
+  if(!birail1)
+    return AY_ENULL;
+
+  if(birail1->npatch)
+    ay_shade_object(togl, birail1->npatch, AY_FALSE);
+
+  if(birail1->start_cap)
+    ay_shade_object(togl, birail1->start_cap, AY_FALSE);
+
+  if(birail1->end_cap)
+    ay_shade_object(togl, birail1->end_cap, AY_FALSE);
+
+ return AY_OK;
+} /* ay_birail1_shadecb */
+
+
+int
+ay_birail1_drawhcb(struct Togl *togl, ay_object *o)
+{
+
+ return AY_OK;
+} /* ay_birail1_drawhcb */
+
+
+int
+ay_birail1_getpntcb(int mode, ay_object *o, double *p)
+{
+
+ return AY_OK;
+} /* ay_birail1_getpntcb */
+
+
+/* Tcl -> C! */
+int
+ay_birail1_setpropcb(Tcl_Interp *interp, int argc, char *argv[], ay_object *o)
+{
+ int ay_status = AY_OK;
+ char *n1 = "Birail1AttrData";
+ /* char fname[] = "birail1_setpropcb";*/
+ Tcl_Obj *to = NULL, *toa = NULL, *ton = NULL;
+ ay_birail1_object *birail1 = NULL;
+
+
+  if(!o)
+    return AY_ENULL;
+
+  birail1 = (ay_birail1_object *)o->refine;
+  
+  toa = Tcl_NewStringObj(n1,-1);
+  ton = Tcl_NewStringObj(n1,-1);
+
+
+  Tcl_SetStringObj(ton,"Rotate",-1);
+  to = Tcl_ObjGetVar2(interp,toa,ton,TCL_LEAVE_ERR_MSG | TCL_GLOBAL_ONLY);
+  Tcl_GetIntFromObj(interp,to, &(birail1->rotate));
+
+  Tcl_SetStringObj(ton,"Close",-1);
+  to = Tcl_ObjGetVar2(interp,toa,ton,TCL_LEAVE_ERR_MSG | TCL_GLOBAL_ONLY);
+  Tcl_GetIntFromObj(interp,to, &(birail1->close));
+
+  Tcl_SetStringObj(ton,"Sections",-1);
+  to = Tcl_ObjGetVar2(interp,toa,ton,TCL_LEAVE_ERR_MSG | TCL_GLOBAL_ONLY);
+  Tcl_GetIntFromObj(interp,to, &(birail1->sections));
+
+  Tcl_SetStringObj(ton,"StartCap",-1);
+  to = Tcl_ObjGetVar2(interp,toa,ton,TCL_LEAVE_ERR_MSG | TCL_GLOBAL_ONLY);
+  Tcl_GetIntFromObj(interp,to, &(birail1->has_start_cap));
+
+  Tcl_SetStringObj(ton,"EndCap",-1);
+  to = Tcl_ObjGetVar2(interp,toa,ton,TCL_LEAVE_ERR_MSG | TCL_GLOBAL_ONLY);
+  Tcl_GetIntFromObj(interp,to, &(birail1->has_end_cap));
+
+  Tcl_SetStringObj(ton,"DisplayMode",-1);
+  to = Tcl_ObjGetVar2(interp,toa,ton,TCL_LEAVE_ERR_MSG | TCL_GLOBAL_ONLY);
+  Tcl_GetIntFromObj(interp,to, &(birail1->glu_display_mode));
+
+  Tcl_SetStringObj(ton,"Tolerance",-1);
+  to = Tcl_ObjGetVar2(interp,toa,ton,TCL_LEAVE_ERR_MSG | TCL_GLOBAL_ONLY);
+  Tcl_GetDoubleFromObj(interp,to, &(birail1->glu_sampling_tolerance));
+
+  Tcl_IncrRefCount(toa);Tcl_DecrRefCount(toa);
+  Tcl_IncrRefCount(ton);Tcl_DecrRefCount(ton);
+
+  ay_status = ay_notify_force(o);
+
+  ay_status = ay_notify_parent();
+
+ return AY_OK;
+} /* ay_birail1_setpropcb */
+
+
+/* C -> Tcl! */
+int
+ay_birail1_getpropcb(Tcl_Interp *interp, int argc, char *argv[], ay_object *o)
+{
+ char *n1="Birail1AttrData";
+ Tcl_Obj *to = NULL, *toa = NULL, *ton = NULL;
+ ay_birail1_object *birail1 = NULL;
+
+  if(!o)
+    return AY_ENULL;
+
+  birail1 = (ay_birail1_object *)(o->refine);
+
+  toa = Tcl_NewStringObj(n1,-1);
+
+  ton = Tcl_NewStringObj(n1,-1);
+
+
+  Tcl_SetStringObj(ton,"Rotate",-1);
+  to = Tcl_NewIntObj(birail1->rotate);
+  Tcl_ObjSetVar2(interp,toa,ton,to,TCL_LEAVE_ERR_MSG |
+		 TCL_GLOBAL_ONLY);
+
+  Tcl_SetStringObj(ton,"Close",-1);
+  to = Tcl_NewIntObj(birail1->close);
+  Tcl_ObjSetVar2(interp,toa,ton,to,TCL_LEAVE_ERR_MSG |
+		 TCL_GLOBAL_ONLY);
+
+  Tcl_SetStringObj(ton,"Sections",-1);
+  to = Tcl_NewIntObj(birail1->sections);
+  Tcl_ObjSetVar2(interp,toa,ton,to,TCL_LEAVE_ERR_MSG |
+		 TCL_GLOBAL_ONLY);
+
+  Tcl_SetStringObj(ton,"StartCap",-1);
+  to = Tcl_NewIntObj(birail1->has_start_cap);
+  Tcl_ObjSetVar2(interp,toa,ton,to,TCL_LEAVE_ERR_MSG |
+		 TCL_GLOBAL_ONLY);
+
+  Tcl_SetStringObj(ton,"EndCap",-1);
+  to = Tcl_NewIntObj(birail1->has_end_cap);
+  Tcl_ObjSetVar2(interp,toa,ton,to,TCL_LEAVE_ERR_MSG |
+		 TCL_GLOBAL_ONLY);
+
+  Tcl_SetStringObj(ton,"DisplayMode",-1);
+  to = Tcl_NewIntObj(birail1->glu_display_mode);
+  Tcl_ObjSetVar2(interp,toa,ton,to,TCL_LEAVE_ERR_MSG |
+		 TCL_GLOBAL_ONLY);
+
+  Tcl_SetStringObj(ton,"Tolerance",-1);
+  to = Tcl_NewDoubleObj(birail1->glu_sampling_tolerance);
+  Tcl_ObjSetVar2(interp,toa,ton,to,TCL_LEAVE_ERR_MSG |
+		 TCL_GLOBAL_ONLY);
+
+  Tcl_IncrRefCount(toa);Tcl_DecrRefCount(toa);
+  Tcl_IncrRefCount(ton);Tcl_DecrRefCount(ton);
+
+ return AY_OK;
+} /* ay_birail1_getpropcb */
+
+
+int
+ay_birail1_readcb(FILE *fileptr, ay_object *o)
+{
+ ay_birail1_object *birail1 = NULL;
+
+ if(!o)
+   return AY_ENULL;
+
+  if(!(birail1 = calloc(1, sizeof(ay_birail1_object))))
+    { return AY_EOMEM; }
+
+  fscanf(fileptr,"%d\n",&birail1->rotate);
+  fscanf(fileptr,"%d\n",&birail1->sections);
+  fscanf(fileptr,"%d\n",&birail1->has_start_cap);
+  fscanf(fileptr,"%d\n",&birail1->has_end_cap);
+  fscanf(fileptr,"%d\n",&birail1->glu_display_mode);
+  fscanf(fileptr,"%lg\n",&birail1->glu_sampling_tolerance);
+
+  if(ay_read_version > 3)
+    {
+      fscanf(fileptr,"%d\n",&birail1->close);
+    }
+
+  o->refine = birail1;
+
+ return AY_OK;
+} /* ay_birail1_readcb */
+
+
+int
+ay_birail1_writecb(FILE *fileptr, ay_object *o)
+{
+ ay_birail1_object *birail1 = NULL;
+
+  if(!o)
+    return AY_ENULL;
+
+  birail1 = (ay_birail1_object *)(o->refine);
+
+  fprintf(fileptr, "%d\n", birail1->rotate);
+  fprintf(fileptr, "%d\n", birail1->sections);
+  fprintf(fileptr, "%d\n", birail1->has_start_cap);
+  fprintf(fileptr, "%d\n", birail1->has_end_cap);
+  fprintf(fileptr, "%d\n", birail1->glu_display_mode);
+  fprintf(fileptr, "%g\n", birail1->glu_sampling_tolerance);
+  fprintf(fileptr, "%d\n", birail1->close);
+
+ return AY_OK;
+} /* ay_birail1_writecb */
+
+
+int
+ay_birail1_wribcb(char *file, ay_object *o)
+{
+ ay_birail1_object *birail1 = NULL;
+
+  if(!o)
+   return AY_ENULL;
+
+  birail1 = (ay_birail1_object*)o->refine;
+
+  if(birail1->npatch)
+    ay_wrib_object(file, birail1->npatch);
+
+  if(birail1->start_cap)
+    ay_wrib_object(file, birail1->start_cap);
+
+  if(birail1->end_cap)
+    ay_wrib_object(file, birail1->end_cap);
+
+ return AY_OK;
+} /* ay_birail1_wribcb */
+
+
+int
+ay_birail1_bbccb(ay_object *o, double *bbox, int *flags)
+{
+ ay_birail1_object *birail1 = NULL;
+
+  if(!o || !bbox)
+    return AY_ENULL;
+
+  birail1 = (ay_birail1_object *)o->refine; 
+
+  if(birail1->npatch)
+    {
+      ay_bbc_get(birail1->npatch, bbox);
+      *flags = 1;
+    }
+  else
+    {
+      /* invalid/nonexisting bbox */
+      *flags = 2;
+    }
+
+ return AY_OK;
+} /* ay_birail1_bbccb */
+
+
+int
+ay_birail1_notifycb(ay_object *o)
+{
+ ay_birail1_object *birail1 = NULL;
+ ay_object *curve1 = NULL, *curve2 = NULL, *pobject1 = NULL, *pobject2 = NULL;
+ ay_object *curve3 = NULL, *pobject3 = NULL;
+ ay_object *npatch = NULL;
+ ay_nurbpatch_object *np = NULL;
+ int ay_status = AY_OK;
+ int got_c1 = AY_FALSE, got_c2 = AY_FALSE, got_c3 = AY_FALSE, mode = 0;
+ double tolerance;
+
+  if(!o)
+    return AY_ENULL;    
+
+  birail1 = (ay_birail1_object *)(o->refine);
+
+  mode = birail1->glu_display_mode;
+  tolerance = birail1->glu_sampling_tolerance;
+
+  /* remove old objects */
+  if(birail1->npatch)
+    ay_object_delete(birail1->npatch);
+  birail1->npatch = NULL;
+
+  if(birail1->start_cap)
+    ay_object_delete(birail1->start_cap);
+  birail1->start_cap = NULL;
+
+  if(birail1->end_cap)
+    ay_object_delete(birail1->end_cap);
+  birail1->end_cap = NULL;
+
+  /* get curves to birail1 */
+  if(!o->down)
+    return AY_OK;
+  curve1 = o->down;
+  if(curve1->type != AY_IDNCURVE)
+    {
+      ay_status = ay_provide_object(curve1, AY_IDNCURVE, &pobject1);
+      if(!pobject1)
+	{
+	  return AY_OK;
+	}
+      else
+	{
+	  curve1 = pobject1;
+	  got_c1 = AY_TRUE;
+	} /* if */
+    } /* if */
+
+  if(!o->down->next)
+    return AY_OK;
+  curve2 = o->down->next;
+  if(curve2->type != AY_IDNCURVE)
+    {
+      ay_status = ay_provide_object(curve2, AY_IDNCURVE, &pobject2);
+      if(!pobject2)
+	{
+	  return AY_OK;
+	}
+      else
+	{
+	  curve2 = pobject2;
+	  got_c2 = AY_TRUE;
+	} /* if */
+    } /* if */
+
+  if(!o->down->next->next)
+    return AY_OK;
+
+  curve3 = o->down->next->next;
+  if(curve3->type != AY_IDNCURVE)
+    {
+      ay_status = ay_provide_object(curve3, AY_IDNCURVE, &pobject3);
+      if(!pobject3)
+	{
+	  return AY_OK;
+	}
+      else
+	{
+	  curve3 = pobject3;
+	  got_c3 = AY_TRUE;
+	} /* if */
+    } /* if */
+
+  /* birail1 */
+  if(!(npatch = calloc(1, sizeof(ay_object))))
+    {
+      return AY_ERROR;
+    }
+
+  ay_object_defaults(npatch);
+  npatch->type = AY_IDNPATCH;
+
+  ay_status = ay_npt_birail1(curve1, curve2, curve3,
+			   birail1->sections, birail1->close,
+			   (ay_nurbpatch_object **)(&(npatch->refine)),
+			   birail1->has_start_cap, &(birail1->start_cap),
+			   birail1->has_end_cap, &(birail1->end_cap));
+
+  if(ay_status)
+    return ay_status;
+
+  /* copy sampling tolerance/mode over to new objects */
+  birail1->npatch = npatch;
+
+  ((ay_nurbpatch_object *)npatch->refine)->glu_sampling_tolerance =
+    tolerance;
+  ((ay_nurbpatch_object *)npatch->refine)->glu_display_mode =
+    mode;
+
+  /* create caps */
+  if(birail1->start_cap)
+    {
+      ((ay_nurbpatch_object *)
+       (birail1->start_cap->refine))->glu_sampling_tolerance = tolerance;
+      ((ay_nurbpatch_object *)
+       (birail1->start_cap->refine))->glu_display_mode = mode;
+    }
+
+  if(birail1->end_cap)
+    {
+      ((ay_nurbpatch_object *)
+       (birail1->end_cap->refine))->glu_sampling_tolerance = tolerance;
+      ((ay_nurbpatch_object *)
+       (birail1->end_cap->refine))->glu_display_mode = mode;
+    }
+
+  /* remove provided objects */
+  if(got_c1)
+    {
+      ay_object_delete(pobject1);
+    }
+
+  if(got_c2)
+    {
+      ay_object_delete(pobject2);
+    }
+
+  if(got_c3)
+    {
+      ay_object_delete(pobject3);
+    }
+
+ return AY_OK;
+} /* ay_birail1_notifycb */
+
+
+int
+ay_birail1_providecb(ay_object *o, unsigned int type, ay_object **result)
+{
+ int ay_status = AY_OK;
+ char fname[] = "birail1_providecb";
+ ay_birail1_object *s = NULL;
+ ay_object *new = NULL, **t = NULL, *p = NULL;
+
+  if(!o)
+    return AY_ENULL;
+
+  if(!result)
+    {
+      if(type == AY_IDNPATCH)
+	return AY_OK;
+      else
+	return AY_ERROR;
+    }
+
+  s = (ay_birail1_object *) o->refine;
+
+  if(type == AY_IDNPATCH)
+    {
+      t = &(new);
+
+      /* copy birail1 */
+      ay_status = ay_object_copy(s->npatch, t);
+      if(ay_status)
+	{
+	  ay_error(ay_status, fname, NULL);
+	  return AY_ERROR;
+	}
+      ay_trafo_copy(o, *t);
+      t = &((*t)->next);
+
+      /* copy caps */
+      p = s->start_cap;
+      while(p)
+	{
+	  ay_status = ay_object_copy(p, t);
+	  if(ay_status)
+	    {
+	      ay_error(ay_status, fname, NULL);
+	      return AY_ERROR;
+	    }
+	  ay_trafo_add(o, *t);
+	  t = &((*t)->next);
+	  p = p->next;
+	} /* while */
+
+      p = s->end_cap;
+      while(p)
+	{
+	  ay_status = ay_object_copy(p, t);
+	  if(ay_status)
+	    {
+	      ay_error(ay_status, fname, NULL);
+	      return AY_ERROR;
+	    }
+	  ay_trafo_add(o, *t);
+	  t = &((*t)->next);
+	  p = p->next;
+	} /* while */
+
+      *result = new;
+    } /* if */
+
+ return ay_status;
+} /* ay_birail1_providecb */
+
+
+int
+ay_birail1_convertcb(ay_object *o, int in_place)
+{
+ int ay_status = AY_OK;
+ ay_birail1_object *r = NULL;
+ ay_object *new = NULL, **next = NULL;
+
+  if(!o)
+    return AY_ENULL;
+
+  /* first, create new objects */
+
+  r = (ay_birail1_object *) o->refine;
+
+  if((r->start_cap) || (r->end_cap))
+    {
+      if(!(new = calloc(1, sizeof(ay_object))))
+	{ return AY_EOMEM; }
+
+      ay_object_defaults(new);
+      new->type = AY_IDLEVEL;
+      new->parent = AY_TRUE;
+      new->inherit_trafos = AY_TRUE;
+      ay_trafo_copy(o, new);
+
+      if(!(new->refine = calloc(1, sizeof(ay_level_object))))
+	{ free(new); return AY_EOMEM; }
+
+      ((ay_level_object *)(new->refine))->type = AY_LTLEVEL;
+
+      next = &(new->down);
+
+      if(r->npatch)
+	{
+	  ay_status = ay_object_copy(r->npatch, next);
+	  if(*next)
+	    {
+	      (*next)->parent = AY_TRUE;
+	      ay_object_crtendlevel(&(*next)->down);
+	      next = &((*next)->next);
+	      next = &((*next)->next);
+	    }
+	}
+
+      if(r->start_cap)
+	{
+	  ay_status = ay_object_copy(r->start_cap, next);
+	  if(*next)
+	    {
+	      next = &((*next)->next);
+	    }
+	}
+
+      if(r->end_cap)
+	{
+	  ay_status = ay_object_copy(r->end_cap, next);
+	  if(*next)
+	    {
+	      next = &((*next)->next);
+	    }
+	}
+
+      ay_object_crtendlevel(next);
+    }
+  else
+    {
+       if(r->npatch)
+	{
+	  ay_status = ay_object_copy(r->npatch, &new);
+	  ay_trafo_copy(o, new);
+	  new->hide_children = AY_TRUE;
+	  new->parent = AY_TRUE;
+	  ay_object_crtendlevel(&(new->down));
+	}
+    } /* if */
+
+  /* second, link new objects, or replace old objects with them */
+
+  if(new)
+    {
+      if(!in_place)
+	{
+	  ay_status = ay_object_link(new);
+	}
+      else
+	{
+	  ay_object_replace(new, o);
+	} /* if */
+    } /* if */
+
+ return ay_status;
+} /* ay_birail1_convertcb */
+
+
+int
+ay_birail1_init(Tcl_Interp *interp)
+{
+ int ay_status = AY_OK;
+
+  ay_status = ay_otype_registercore(ay_birail1_name,
+				    ay_birail1_createcb,
+				    ay_birail1_deletecb,
+				    ay_birail1_copycb,
+				    ay_birail1_drawcb,
+				    NULL, /* no handles */
+				    ay_birail1_shadecb,
+				    ay_birail1_setpropcb,
+				    ay_birail1_getpropcb,
+				    ay_birail1_getpntcb,
+				    ay_birail1_readcb,
+				    ay_birail1_writecb,
+				    ay_birail1_wribcb,
+				    ay_birail1_bbccb,
+				    AY_IDBIRAIL1);
+
+  
+  ay_status = ay_notify_register(ay_birail1_notifycb, AY_IDBIRAIL1);
+
+  ay_status = ay_convert_register(ay_birail1_convertcb, AY_IDBIRAIL1);
+
+  ay_status = ay_provide_register(ay_birail1_providecb, AY_IDBIRAIL1);
+
+ return ay_status;
+} /* ay_birail1_init */
+
---
+++ ayam/src/tcl/birail1.tcl
@@ -0,0 +1,70 @@
+# Ayam, a free 3D modeler for the RenderMan interface.
+#
+# Ayam is copyrighted 1998-2004 by Randolf Schultz
+# (rschultz@informatik.uni-rostock.de) and others.
+#
+# All rights reserved.
+#
+# See the file License for details.
+
+# birail1.tcl - interpolating curves objects Tcl code
+
+set Birail1_props { Transformations Attributes Material Tags Birail1Attr }
+
+
+array set Birail1Attr {
+arr   Birail1AttrData
+sproc ""
+gproc ""
+w     fBirail1Attr
+
+}
+
+array set Birail1AttrData {
+DisplayMode 1
+}
+# create Birail1Attr-UI
+set w [frame $ay(pca).$Birail1Attr(w)]
+
+
+addCheck $w Birail1AttrData Rotate
+addCheck $w Birail1AttrData Interpolate
+addCheck $w Birail1AttrData Close
+addParam $w Birail1AttrData Sections
+addCheck $w Birail1AttrData StartCap
+addCheck $w Birail1AttrData EndCap
+global ay
+set l $ay(npdisplaymodes)
+set l [linsert $l 0 Global]
+addMenu $w Birail1AttrData DisplayMode $l
+addParam $w Birail1AttrData Tolerance
+
+
+#birail1_crt:
+#
+#
+proc birail1_crt { } {
+    global ay ay_error selected
+    set selected ""
+    getSel selected
+    if { $selected == "" } { return; }
+    set ay_error 0
+    crtOb Birail1
+    if { $ay_error } {  return; }
+
+    cutOb
+    set ay(ul) $ay(CurrentLevel)
+    uS
+    sL
+    getLevel a b
+    goDown [expr [llength $a]-1]
+    cmovOb
+    goUp
+    set ay(ul) $ay(CurrentLevel)
+    uS
+    sL
+    rV
+
+ return;
+}
+# birail1_crt
