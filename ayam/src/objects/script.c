49ab869af0c5dac055a4d82a3e5fc8012d5fa9c0
Randolf Schultz <randolf.schultz@gmail.com>
2005-01-08 18:53:04 +0000

    added option to break scripts with endless loops

--- ayam/src/objects/script.c
+++ ayam/src/objects/script.c
@@ -12,6 +12,8 @@
 
 #include "ayam.h"
 
+#include <Xutil.h>
+
 /* script.c - script object */
 
 /*
@@ -697,6 +699,37 @@ Tk_RestrictAction
 ay_script_restrictall(ClientData clientData,
 		      XEvent *eventPtr)
 {
+ XKeyEvent *key_event = (XKeyEvent *) eventPtr;
+ KeySym ks;
+ XComposeStatus status;
+ char tmpstr[128];
+ Tcl_Obj *to = NULL, *ton = NULL;
+
+  if(eventPtr->type == KeyPress)
+    {
+      if(key_event->state & (ControlMask|ShiftMask))
+	{
+
+	  XLookupString(key_event, tmpstr, 128, &ks, &status);
+	  if(ks == 0x43)
+	    {
+	      ton = Tcl_NewStringObj("cancelled", -1);
+	      to = Tcl_NewIntObj(1);
+	      Tcl_ObjSetVar2(ay_interp, ton, NULL, to, TCL_LEAVE_ERR_MSG |
+			     TCL_GLOBAL_ONLY);
+	      return TK_DISCARD_EVENT;
+	    }
+	  else
+	    {
+	      return TK_DISCARD_EVENT;
+	    } /* if */
+	}
+      else
+	{
+	  return TK_DISCARD_EVENT;
+	} /* if */
+    } /* if */
+
   return TK_DEFER_EVENT;
 } /* ay_script_restrictall */
 
