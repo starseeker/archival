e5e35e9fa1bd55ed8d0481a7bc0b92153cd7a35a
Randolf Schultz <randolf.schultz@gmail.com>
2005-03-03 17:38:25 +0000

    added support for export to 3dm files

--- ayam/src/plugins/onio.tcl
+++ ayam/src/plugins/onio.tcl
@@ -102,7 +102,96 @@ proc onio_import { } {
 }
 # onio_import
 
-# link proc onio_import to Custom menu
+proc onio_export { } {
+    global ay
+
+    set filename $ay(filename)
+
+    set types {{"3DM (Rhino) Files" ".3dm"} {"All files" *}}
+
+    if { $filename == "" } {
+	set dirname [pwd]
+    } else {
+	set dirname [file dirname $filename]
+	if { $dirname == "." } { set dirname [pwd] }
+    }
+
+    set efilename [tk_getSaveFile -filetypes $types -parent .\
+	    -initialfile [file tail $filename] -initialdir $dirname\
+	    -title "Select file to export:"]
+
+    if { $efilename != "" } {
+	cS; plb_update
+	update
+	global ay ay_error onio_options
+	set ay_error ""
+	set onio_options(FileName) $efilename
+	set w .onio
+	catch {destroy $w}
+	toplevel $w -class ayam
+	wm title $w "OpenNURBS IO Options"
+	wm iconname $w "Ayam"
+	wm transient $w .
+
+	set f [frame $w.f1]
+	pack $f -in $w -side top -fill x
+
+	addParam $f onio_options Accuracy
+
+	set f [frame $w.f2]
+	button $f.bok -text "Ok" -width 5 -command {
+	    global onio_options;
+	    set oldcd [pwd]
+	    cd [file dirname $onio_options(FileName)]
+
+	    onioWrite [file tail $onio_options(FileName)]
+
+	    cd $oldcd
+	    goTop
+	    selOb
+	    set ay(CurrentLevel) "root"
+	    set ay(SelectedLevel) "root"
+	    update
+
+	    uS
+	    rV
+
+	    set ay(sc) 1
+
+	    if { $ay_error < 2 } {
+		ayError 4 "onio_import" "Done exporting to:"
+		ayError 4 "onio_import" "$onio_options(FileName)"
+	    } else {
+		ayError 2 "Ayam" "There were errors while exporting to:"
+		ayError 2 "Ayam" "$onio_options(FileName)"
+	    }
+
+	    grab release .onio;
+	    focus .;
+	    destroy .onio
+	}
+
+	button $f.bca -text "Cancel" -width 5 -command "\
+		grab release .onio;\
+		focus .;\
+		destroy .onio"
+
+	pack $f.bok $f.bca -in $f -side left -fill x -expand yes
+	pack $f -in $w -side bottom -fill x
+
+	winCenter $w
+	grab $w
+	focus $w.f2.bok
+	tkwait window $w
+
+	after idle viewMouseToCurrent
+    }
+
+ return;
+}
+# onio_export
+
+# link proc onio_import and onio_export to Custom menu
 # we need access to global array "ay"
 global ay
 # always create a cascaded sub-menu
@@ -112,3 +201,4 @@ set m [menu $ay(cm).onio]
 # create menu entry
 $m add command -label "Import 3DM (Rhino)" -command onio_import
 
+$m add command -label "Export 3DM (Rhino)" -command onio_export
