25222d73d48b2f82e1e18481dc182476e0895156
Randolf Schultz <randolf.schultz@gmail.com>
2005-04-16 08:06:36 +0000

    fixed provide and convert callbacks

--- ayam/src/objects/extrnc.c
+++ ayam/src/objects/extrnc.c
@@ -404,7 +404,7 @@ ay_extrnc_providecb(ay_object *o, unsigned int type, ay_object **result)
 	  ay_error(ay_status, fname, NULL);
 	  return AY_ERROR;
 	}
-      ay_trafo_copy(o, *t);
+      ay_trafo_add(o, *t);
       t = &((*t)->next);
 
       *result = new;
@@ -430,7 +430,7 @@ ay_extrnc_convertcb(ay_object *o, int in_place)
   if(r->ncurve)
     {
       ay_status = ay_object_copy(r->ncurve, &new);
-      ay_trafo_copy(o, new);
+      ay_trafo_add(o, new);
     } /* if */
 
   /* second, link new object, or replace old object with it */
