aab1c34a4289c916c999067b03f5f6fe5091c818
Randolf Schultz <randolf.schultz@gmail.com>
2005-04-27 20:00:37 +0000

    added provide and convert callbacks

--- ayam/src/objects/box.c
+++ ayam/src/objects/box.c
@@ -385,6 +385,217 @@ ay_box_bbccb(ay_object *o, double *bbox, int *flags)
 
 
 int
+ay_box_providecb(ay_object *o, unsigned int type, ay_object **result)
+{
+ int ay_status = AY_OK;
+ int stride = 4, i, j;
+ double *controlv = NULL;
+ double wh = 0.0, lh = 0.0, hh = 0.0;
+ ay_box_object *box = NULL;
+ ay_bpatch_object bpatch = {{0}};
+ ay_object *new = NULL, d = {0}, **n = NULL;
+ ay_nurbpatch_object *np = NULL;
+
+  if(!o)
+    return AY_ENULL;
+
+  if(!result)
+    {
+      if(type == AY_IDNPATCH)
+	return AY_OK;
+      else
+	return AY_ERROR;
+    }
+
+  box = (ay_box_object *) o->refine;
+
+  wh = box->width  * 0.5;
+  lh = box->length * 0.5;
+  hh = box->height * 0.5;
+
+  if(type == AY_IDNPATCH)
+    {
+      if(!(controlv = calloc(1, 5*2*stride*sizeof(double))))
+	{
+	  ay_status = AY_EOMEM;
+	  goto cleanup;
+	}
+
+      /* set x */
+      controlv[0] = wh;
+      controlv[4] = -wh;
+      controlv[8] = -wh;
+      controlv[12] = wh;
+      controlv[16] = wh;
+      controlv[20] = wh;
+      controlv[24] = -wh;
+      controlv[28] = -wh;
+      controlv[32] = wh;
+      controlv[36] = wh;
+
+      /* set y */
+      j = 1;
+      for(i = 0; i < 5; i++)
+	{
+	  controlv[j] = hh;
+	  controlv[j+5*stride] = -hh;
+	  j += stride;
+	}
+
+      /* set z */
+      controlv[2] = lh;
+      controlv[6] = lh;
+      controlv[10] = -lh;
+      controlv[14] = -lh;
+      controlv[18] = lh;
+      controlv[22] = lh;
+      controlv[26] = lh;
+      controlv[30] = -lh;
+      controlv[34] = -lh;
+      controlv[38] = lh;
+
+      /* set w eights */
+      j = 3;
+      for(i = 0; i < 10; i++)
+	{
+	  controlv[j] = 1.0;
+	  j += stride;
+	}
+
+      ay_status = ay_npt_create(2, 2, 2, 5, AY_KTBEZIER, AY_KTNURB,
+				controlv, NULL, NULL, &np);
+
+      if(ay_status)
+	goto cleanup;
+
+      if(!(new = calloc(1, sizeof(ay_object))))
+	{
+	  ay_status = AY_EOMEM;
+	  goto cleanup;
+	}
+
+      ay_object_defaults(new);
+      new->type = AY_IDNPATCH;
+      new->inherit_trafos = AY_FALSE;
+      new->parent = AY_TRUE;
+      new->hide_children = AY_TRUE;
+
+      ay_status = ay_object_crtendlevel(&(new->down));
+      if(ay_status)
+	goto cleanup;
+
+      ay_trafo_copy(o, new);
+      new->refine = np;
+      n = &(new->next);
+
+      /* create upper cap */
+      ay_object_defaults(&d);
+      d.type = AY_IDBPATCH;
+      d.refine = &bpatch;
+      memcpy(bpatch.p1, &(controlv[0*stride]), 3*sizeof(double));
+      memcpy(bpatch.p2, &(controlv[1*stride]), 3*sizeof(double));
+      memcpy(bpatch.p3, &(controlv[2*stride]), 3*sizeof(double));
+      memcpy(bpatch.p4, &(controlv[3*stride]), 3*sizeof(double));
+      ay_provide_object(&d, AY_IDNPATCH, n);
+      if(*n)
+	{
+	  ay_trafo_copy(o, *n);
+	  n = &((*n)->next);
+	}
+      /* create lower cap */
+      memcpy(bpatch.p1, &(controlv[6*stride]), 3*sizeof(double));
+      memcpy(bpatch.p2, &(controlv[7*stride]), 3*sizeof(double));
+      memcpy(bpatch.p3, &(controlv[8*stride]), 3*sizeof(double));
+      memcpy(bpatch.p4, &(controlv[9*stride]), 3*sizeof(double));
+      ay_provide_object(&d, AY_IDNPATCH, n);
+      if(*n)
+	{
+	  ay_trafo_copy(o, *n);
+	  n = &((*n)->next);
+	}
+
+      /* return result */
+      *result = new;
+
+      controlv = NULL; np = NULL; new = NULL;
+
+    } /* if */
+
+
+cleanup:
+
+  if(controlv)
+    free(controlv);
+
+  if(np)
+    free(np);
+
+  if(new)
+    {
+      ay_object_deletemulti(new);
+    }
+
+ return ay_status;
+} /* ay_box_providecb */
+
+
+int
+ay_box_convertcb(ay_object *o, int in_place)
+{
+ int ay_status = AY_OK;
+ ay_object *new = NULL, *t;
+ ay_box_object *box = NULL;
+
+  if(!o)
+    return AY_ENULL;
+
+  box = (ay_box_object *) o->refine;
+
+  /* first, create new objects */
+
+  if(!(new = calloc(1, sizeof(ay_object))))
+    { return AY_EOMEM; }
+
+  ay_object_defaults(new);
+  new->type = AY_IDLEVEL;
+  new->parent = AY_TRUE;
+  new->inherit_trafos = AY_TRUE;
+
+  if(!(new->refine = calloc(1, sizeof(ay_level_object))))
+    { free(new); return AY_EOMEM; }
+
+  ((ay_level_object *)(new->refine))->type = AY_LTLEVEL;
+
+  ay_status = ay_box_providecb(o, AY_IDNPATCH, &new->down);
+  if(ay_status)
+    { free(new->refine); free(new); return ay_status; }
+
+  t = new->down;
+  while(t->next)
+    {
+      t = t->next;
+    }
+  ay_status = ay_object_crtendlevel(&(t->next));
+
+  /* second, link new objects, or replace old object with them */
+
+  if(new)
+    {
+      if(!in_place)
+	{
+	  ay_status = ay_object_link(new);
+	}
+      else
+	{
+	  ay_object_replace(new, o);
+	} /* if */
+    } /* if */
+
+ return ay_status;
+} /* ay_box_convertcb */
+
+
+int
 ay_box_init(Tcl_Interp *interp)
 {
  int ay_status = AY_OK;
@@ -404,6 +615,10 @@ ay_box_init(Tcl_Interp *interp)
 				    ay_box_wribcb,
 				    ay_box_bbccb,
 				    AY_IDBOX);
+
+  ay_status = ay_convert_register(ay_box_convertcb, AY_IDBOX);
+
+  ay_status = ay_provide_register(ay_box_providecb, AY_IDBOX);
 
  return ay_status;
 } /* ay_box_init */
