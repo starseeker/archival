2f7eb3e904ffafa6449c627bdc6b08af3aabb70d
Randolf Schultz <randolf.schultz@gmail.com>
2005-05-26 18:18:51 +0000

    improved dialogs

--- ayam/src/plugins/onio.tcl
+++ ayam/src/plugins/onio.tcl
@@ -7,7 +7,7 @@
 #
 # See the file License for details.
 
-# rrib.tcl - onio (OpenNURBS 3DM Rhino) plugin GUI code
+# onio.tcl - onio (OpenNURBS 3DM Rhino) plugin GUI code
 
 uplevel #0 { array set onio_options {
     Accuracy 1.0e-12
@@ -16,205 +16,185 @@ uplevel #0 { array set onio_options {
     IgnoreFirstTrim 1
     WriteCurves 1
     QuadAsBRep 1
+    FileName "unnamed.3dm"
 }   }
 
-proc onio_import { } {
-    global ay
 
-    set filename $ay(filename)
+#
+proc onio_import { } {
+    global ay ay_error onio_options
+
+    cS; plb_update
+    update
+
+    set ay_error ""
+
+    set w .onio
+    catch {destroy $w}
+    toplevel $w -class ayam
+    wm title $w "OpenNURBS Import Options"
+    wm iconname $w "Ayam"
+    wm transient $w .
+
+    set f [frame $w.f1]
+    pack $f -in $w -side top -fill x
 
     set types {{"3DM (Rhino) Files" ".3dm"} {"All files" *}}
+    addFileT $f onio_options FileName $types
 
-    if { $filename == "" } {
-	set dirname [pwd]
-    } else {
-	set dirname [file dirname $filename]
-	if { $dirname == "." } { set dirname [pwd] }
-    }
+    addParam $f onio_options Accuracy [list 0.0 1.0e-12 0.1 1]
+    addCheck $f onio_options ReadCurves
+    addParam $f onio_options ReadLayers [list -1 1 1-10]
+    addCheck $f onio_options IgnoreFirstTrim
 
-    set ifilename [tk_getOpenFile -filetypes $types -parent .\
-	    -initialfile [file tail $filename] -initialdir $dirname\
-	    -title "Select file to import:"]
+    set f [frame $w.f2]
+    button $f.bok -text "Ok" -width 5 -command {
+	global onio_options
 
-    if { $ifilename != "" } {
-	cS; plb_update
+	set oldcd [pwd]
+	cd [file dirname $onio_options(FileName)]
+
+	onioRead [file tail $onio_options(FileName)]\
+	    -a $onio_options(Accuracy)\
+	    -c $onio_options(ReadCurves)\
+	    -l $onio_options(ReadLayers)\
+	    -i $onio_options(IgnoreFirstTrim)
+
+	cd $oldcd
+	goTop
+	selOb
+	set ay(CurrentLevel) "root"
+	set ay(SelectedLevel) "root"
 	update
-	global ay ay_error onio_options
-	set ay_error ""
-	set onio_options(FileName) $ifilename
-	set w .onio
-	catch {destroy $w}
-	toplevel $w -class ayam
-	wm title $w "OpenNURBS IO Options"
-	wm iconname $w "Ayam"
-	wm transient $w .
 
-	set f [frame $w.f1]
-	pack $f -in $w -side top -fill x
+	uS
+	rV
 
-	addParam $f onio_options Accuracy [list 0.0 1.0e-12 0.1 1]
-	addCheck $f onio_options ReadCurves
-	addParam $f onio_options ReadLayers [list -1 1 1-10]
-	addCheck $f onio_options IgnoreFirstTrim
+	set ay(sc) 1
 
-	set f [frame $w.f2]
-	button $f.bok -text "Ok" -width 5 -command {
-	    global onio_options;
-	    set oldcd [pwd]
-	    cd [file dirname $onio_options(FileName)]
-
-	    onioRead [file tail $onio_options(FileName)]\
-		    -a $onio_options(Accuracy)\
-		    -c $onio_options(ReadCurves)\
-		    -l $onio_options(ReadLayers)\
-		    -i $onio_options(IgnoreFirstTrim)
-
-	    cd $oldcd
-	    goTop
-	    selOb
-	    set ay(CurrentLevel) "root"
-	    set ay(SelectedLevel) "root"
-	    update
-
-	    uS
-	    rV
-
-	    set ay(sc) 1
-
-	    if { $ay_error < 2 } {
-		ayError 4 "onio_import" "Done importing:"
-		ayError 4 "onio_import" "$onio_options(FileName)"
-	    } else {
-		ayError 2 "Ayam" "There were errors while importing:"
-		ayError 2 "Ayam" "$onio_options(FileName)"
-	    }
-
-	    grab release .onio;
-	    focus .;
-	    destroy .onio
+	if { $ay_error < 2 } {
+	    ayError 4 "onio_import" "Done importing:"
+	    ayError 4 "onio_import" "$onio_options(FileName)"
+	} else {
+	    ayError 2 "onio_import" "There were errors while importing:"
+	    ayError 2 "onio_import" "$onio_options(FileName)"
 	}
 
-	button $f.bca -text "Cancel" -width 5 -command "\
+	grab release .onio;
+	focus .;
+	destroy .onio
+    }
+    # button
+
+    button $f.bca -text "Cancel" -width 5 -command "\
 		grab release .onio;\
 		focus .;\
 		destroy .onio"
 
-	pack $f.bok $f.bca -in $f -side left -fill x -expand yes
-	pack $f -in $w -side bottom -fill x
+    pack $f.bok $f.bca -in $f -side left -fill x -expand yes
+    pack $f -in $w -side bottom -fill x
 
-	winCenter $w
-	grab $w
-	focus $w.f2.bok
-	tkwait window $w
+    winCenter $w
+    grab $w
+    focus $w.f2.bok
+    tkwait window $w
 
-	after idle viewMouseToCurrent
-    }
+    after idle viewMouseToCurrent
 
  return;
 }
 # onio_import
 
+
 proc onio_export { } {
-    global ay
+    global ay ay_error onio_options
+
+    cS; plb_update
+    update
+
+    set ay_error ""
+
+    set w .onio
+    catch {destroy $w}
+    toplevel $w -class ayam
+    wm title $w "OpenNURBS Export Options"
+    wm iconname $w "Ayam"
+    wm transient $w .
+
+    set f [frame $w.f1]
+    pack $f -in $w -side top -fill x
 
     set types {{"3DM (Rhino) Files" ".3dm"} {"All files" *}}
+    addSFileT $f onio_options FileName $types
 
-    if { $ay(filename) == "" } {
-	set filename "unnamed.3dm"
-	set dirname [pwd]
-    } else {
-	set dirname [file dirname $ay(filename)]
-	if { $dirname == "." } { set dirname [pwd] }
-	set filename [file rootname $ay(filename)].3dm
-    }
+    addParam $f onio_options Accuracy
+    addCheck $f onio_options WriteCurves
+    addCheck $f onio_options QuadAsBRep
 
-    set efilename [tk_getSaveFile -filetypes $types -parent .\
-	    -initialfile [file tail $filename] -initialdir $dirname\
-	    -title "Select file to export:"]
+    set f [frame $w.f2]
+    button $f.bok -text "Ok" -width 5 -command {
+	global onio_options;
+	set oldcd [pwd]
+	cd [file dirname $onio_options(FileName)]
 
-    if { $efilename != "" } {
-	cS; plb_update
+	onioWrite [file tail $onio_options(FileName)]\
+	    -c $onio_options(WriteCurves)\
+	    -q $onio_options(QuadAsBRep)
+
+	cd $oldcd
+	goTop
+	selOb
+	set ay(CurrentLevel) "root"
+	set ay(SelectedLevel) "root"
 	update
-	global ay ay_error onio_options
-	set ay_error ""
-	set onio_options(FileName) $efilename
-	set w .onio
-	catch {destroy $w}
-	toplevel $w -class ayam
-	wm title $w "OpenNURBS IO Options"
-	wm iconname $w "Ayam"
-	wm transient $w .
 
-	set f [frame $w.f1]
-	pack $f -in $w -side top -fill x
+	uS
+	rV
 
-	addParam $f onio_options Accuracy
-	addCheck $f onio_options WriteCurves
-	addCheck $f onio_options QuadAsBRep
+	set ay(sc) 1
 
-	set f [frame $w.f2]
-	button $f.bok -text "Ok" -width 5 -command {
-	    global onio_options;
-	    set oldcd [pwd]
-	    cd [file dirname $onio_options(FileName)]
-
-	    onioWrite [file tail $onio_options(FileName)]\
-		    -c $onio_options(WriteCurves)\
-		    -q $onio_options(QuadAsBRep)
-
-	    cd $oldcd
-	    goTop
-	    selOb
-	    set ay(CurrentLevel) "root"
-	    set ay(SelectedLevel) "root"
-	    update
-
-	    uS
-	    rV
-
-	    set ay(sc) 1
-
-	    if { $ay_error < 2 } {
-		ayError 4 "onio_import" "Done exporting to:"
-		ayError 4 "onio_import" "$onio_options(FileName)"
-	    } else {
-		ayError 2 "Ayam" "There were errors while exporting to:"
-		ayError 2 "Ayam" "$onio_options(FileName)"
-	    }
-
-	    grab release .onio;
-	    focus .;
-	    destroy .onio
+	if { $ay_error < 2 } {
+	    ayError 4 "onio_export" "Done exporting to:"
+	    ayError 4 "onio_export" "$onio_options(FileName)"
+	} else {
+	    ayError 2 "onio_export" "There were errors while exporting to:"
+	    ayError 2 "onio_export" "$onio_options(FileName)"
 	}
+	# if
 
-	button $f.bca -text "Cancel" -width 5 -command "\
+	grab release .onio;
+	focus .;
+	destroy .onio
+    }
+    # button
+
+    button $f.bca -text "Cancel" -width 5 -command "\
 		grab release .onio;\
 		focus .;\
 		destroy .onio"
 
-	pack $f.bok $f.bca -in $f -side left -fill x -expand yes
-	pack $f -in $w -side bottom -fill x
+    pack $f.bok $f.bca -in $f -side left -fill x -expand yes
+    pack $f -in $w -side bottom -fill x
 
-	winCenter $w
-	grab $w
-	focus $w.f2.bok
-	tkwait window $w
+    winCenter $w
+    grab $w
+    focus $w.f2.bok
+    tkwait window $w
 
-	after idle viewMouseToCurrent
-    }
+    after idle viewMouseToCurrent
 
  return;
 }
 # onio_export
 
-# link proc onio_import and onio_export to Custom menu
+# link procs onio_import and onio_export to Custom menu
 # we need access to global array "ay"
 global ay
 # always create a cascaded sub-menu
 $ay(cm) add cascade -menu $ay(cm).onio -label "OpenNURBS IO"
-# create menu
+# create sub-menu
 set m [menu $ay(cm).onio]
-# create menu entry
+# create sub-menu entries
 $m add command -label "Import 3DM (Rhino)" -command onio_import
 
 $m add command -label "Export 3DM (Rhino)" -command onio_export
--- ayam/src/plugins/rrib.tcl
+++ ayam/src/plugins/rrib.tcl
@@ -10,127 +10,116 @@
 # rrib.tcl - RRIB plugin GUI code
 
 uplevel #0 { array set rrib_options {
-ReadFrame -1
-ReadCamera 1
-ReadOptions 1
-ReadLights 1
-ReadMaterial 1
-ReadPartial 0
-ErrorLevel 1
+  ReadFrame -1
+  ReadCamera 1
+  ReadOptions 1
+  ReadLights 1
+  ReadMaterial 1
+  ReadPartial 0
+  ErrorLevel 1
+  FileName "unnamed.rib"
 }   }
 
 # rrib_import:
 #
 #
 proc rrib_import { } {
-    global ay
+    global ay ay_error rrib_options
 
-    set filename $ay(filename)
+    cS; plb_update
+    update
+
+    set ay_error ""
+
+    set w .ribI
+    catch {destroy $w}
+    toplevel $w -class ayam
+    wm title $w "RRIB Options"
+    wm iconname $w "Ayam"
+    wm transient $w .
+
+    set f [frame $w.f1]
+    pack $f -in $w -side top -fill x
 
     set types {{"RIB Files" ".rib"} {"All files" *}}
+    addFileT $f rrib_options FileName $types
 
-    if { $filename == "" } {
-	set dirname [pwd]
-    } else {
-	set dirname [file dirname $filename]
-	if { $dirname == "." } { set dirname [pwd] }
-    }
+    addParam $f rrib_options ReadFrame
+    addCheck $f rrib_options ReadCamera
+    addCheck $f rrib_options ReadOptions
+    addCheck $f rrib_options ReadLights
+    addCheck $f rrib_options ReadMaterial
+    addCheck $f rrib_options ReadPartial
+    addMenu $f rrib_options ErrorLevel [list Silence Errors Warnings All]
 
-    set ifilename [tk_getOpenFile -filetypes $types -parent .\
-	    -initialfile [file tail $filename] -initialdir $dirname\
-	    -title "Select file to import:"]
+    set f [frame $w.f2]
+    button $f.bok -text "Ok" -width 5 -command {
+	global rrib_options
+	set oldcd [pwd]
+	cd [file dirname $rrib_options(FileName)]
 
-    if { $ifilename != "" } {
-	cS; plb_update
+	rrib [file tail $rrib_options(FileName)]\
+	    -f $rrib_options(ReadFrame)\
+	    -c $rrib_options(ReadCamera)\
+	    -o $rrib_options(ReadOptions)\
+	    -l $rrib_options(ReadLights)\
+	    -m $rrib_options(ReadMaterial)\
+	    -p $rrib_options(ReadPartial)\
+	    -e $rrib_options(ErrorLevel);
+
+	cd $oldcd
+	goTop
+	selOb
+	set ay(CurrentLevel) "root"
+	set ay(SelectedLevel) "root"
 	update
-	global ay ay_error rrib_options
-	set ay_error ""
-	set rrib_options(FileName) $ifilename
-	set w .ribI
-	catch {destroy $w}
-	toplevel $w -class ayam
-	wm title $w "RRIB Options"
-	wm iconname $w "Ayam"
-	wm transient $w .
 
-	set f [frame $w.f1]
-	pack $f -in $w -side top -fill x
+	uS
+	rV
 
-	addParam $f rrib_options ReadFrame
-	addCheck $f rrib_options ReadCamera
-	addCheck $f rrib_options ReadOptions
-	addCheck $f rrib_options ReadLights
-	addCheck $f rrib_options ReadMaterial
-	addCheck $f rrib_options ReadPartial
-	addMenu $f rrib_options ErrorLevel [list Silence Errors Warnings All]
+	set ay(sc) 1
 
-	set f [frame $w.f2]
-	button $f.bok -text "Ok" -width 5 -command {
-	    global rrib_options;
-	    set oldcd [pwd]
-	    cd [file dirname $rrib_options(FileName)]
-
-	    rrib [file tail $rrib_options(FileName)]\
-		    -f $rrib_options(ReadFrame)\
-		    -c $rrib_options(ReadCamera)\
-		    -o $rrib_options(ReadOptions)\
-		    -l $rrib_options(ReadLights)\
-		    -m $rrib_options(ReadMaterial)\
-		    -p $rrib_options(ReadPartial)\
-		    -e $rrib_options(ErrorLevel);
-
-	    cd $oldcd
-	    goTop
-	    selOb
-	    set ay(CurrentLevel) "root"
-	    set ay(SelectedLevel) "root"
-	    update
-
-	    uS
-	    rV
-
-	    set ay(sc) 1
-
-	    if { $ay_error < 2 } {
-		ayError 4 "rrib_import" "Done importing:"
-		ayError 4 "rrib_import" "$rrib_options(FileName)"
-	    } else {
-		ayError 2 "Ayam" "There were errors while importing:"
-		ayError 2 "Ayam" "$rrib_options(FileName)"
-	    }
-
-
-	    grab release .ribI;
-	    focus .;
-	    destroy .ribI
+	if { $ay_error < 2 } {
+	    ayError 4 "rrib_import" "Done importing:"
+	    ayError 4 "rrib_import" "$rrib_options(FileName)"
+	} else {
+	    ayError 2 "rrib_import" "There were errors while importing:"
+	    ayError 2 "rrib_import" "$rrib_options(FileName)"
 	}
 
-	button $f.bca -text "Cancel" -width 5 -command "\
+
+	grab release .ribI;
+	focus .;
+	destroy .ribI
+    }
+    # button
+
+    button $f.bca -text "Cancel" -width 5 -command "\
 		grab release .ribI;\
 		focus .;\
 		destroy .ribI"
 
-	pack $f.bok $f.bca -in $f -side left -fill x -expand yes
-	pack $f -in $w -side bottom -fill x
+    pack $f.bok $f.bca -in $f -side left -fill x -expand yes
+    pack $f -in $w -side bottom -fill x
 
-	winCenter $w
-	grab $w
-	focus $w.f2.bok
-	tkwait window $w
+    winCenter $w
+    grab $w
+    focus $w.f2.bok
+    tkwait window $w
 
-	after idle viewMouseToCurrent
-    }
+    after idle viewMouseToCurrent
 
  return;
 }
 # rrib_import
+
 
 # link proc rrib_import to Custom menu
 # we need access to global array "ay"
 global ay
 # always create a cascaded sub-menu
 $ay(cm) add cascade -menu $ay(cm).rrib -label "RRIB"
-# create menu
+# create sub-menu
 set m [menu $ay(cm).rrib]
-# create menu entry
+# create sub-menu entry
 $m add command -label "Import RIB" -command rrib_import
