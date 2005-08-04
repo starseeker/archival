afc4ec7da19da1e50e95e7383e2e1e7a771aa80d
Randolf Schultz <randolf.schultz@gmail.com>
2005-08-04 15:36:30 +0000

    changed Mops import dialog to new style

--- ayam/src/tcl/io.tcl
+++ ayam/src/tcl/io.tcl
@@ -434,104 +434,90 @@ proc io_loadCustom { } {
 uplevel #0 { array set mopsi_options {
 ResetDM 1
 ResetST 1
-ifilename ""
+filename ""
 }   }
 
 # io_importMops:
 #  import a Mops scene file
 proc io_importMops { } {
-    global ay tcl_platform
+    global ay ay_error mopsi_options tcl_platform
 
-    set filename $ay(filename)
+    if { $mopsi_options(filename) != "" } {
+	set mopsi_options(FileName) $mopsi_options(filename)
+    } else {
+	set mopsi_options(FileName) "unnamed.mop"
+    }
+
+    set w .mopI
+    catch {destroy $w}
+    toplevel $w -class ayam
+    wm title $w "Import Options"
+    wm iconname $w "Ayam"
+    wm transient $w .
+
+    set f [frame $w.f1]
+    pack $f -in $w -side top -fill x
 
     set types {{"Mops Scene" ".mop"} {"All files" *}}
 
-    if { $filename == "" } {
-	set dirname [pwd]
-    } else {
-	set dirname [file dirname $filename]
-	if { $dirname == "." } { set dirname [pwd] }
-    }
-    set types {{"Mops Scene" ".mop"} {"All files" *}}
+    addFileT $f mopsi_options FileName $types
+    addCheckB $f mopsi_options ResetDM [ms mopsi_options_ResetDM]
+    addCheckB $f mopsi_options ResetST [ms mopsi_options_ResetST]
 
-    if { $tcl_platform(os) != "Darwin" } {
-	set ifilename [tk_getOpenFile -filetypes $types -parent .\
-		-initialfile [file tail $filename] -initialdir $dirname\
-		-title "Select file to import:"]
-    } else {
-	set ifilename [tk_getOpenFile -filetypes $types -parent .\
-		-initialfile [file tail $filename]\
-		-title "Select file to import:"]
-    }
+    set f [frame $w.f2]
+    button $f.bok -text "Ok" -width 5 -command {
+	global mopsi_options ay_error
 
-    if { $ifilename != "" } {
-	global ay_error mopsi_options
+	cS; plb_update
+
 	set ay_error ""
-	set mopsi_options(ifilename) $ifilename
 
-	set w .mopI
-	catch {destroy $w}
-	toplevel $w -class ayam
-	wm title $w "Import Options"
-	wm iconname $w "Ayam"
-	wm transient $w .
+	update
+	foreach view $ay(views) { viewUnBind $view }
+	update
 
-	set f [frame $w.f1]
-	pack $f -in $w -side top -fill x
+	set filename $mopsi_options(FileName)
+	importMops $filename
 
-	addCheckB $f mopsi_options ResetDM [ms mopsi_options_ResetDM]
-	addCheckB $f mopsi_options ResetST [ms mopsi_options_ResetST]
-
-	set f [frame $w.f2]
-	button $f.bok -text "Ok" -width 5 -command {
-	    global mopsi_options ay_error
-	    cS; plb_update
-	    set ay_error ""
-	    update
-	    foreach view $ay(views) { viewUnBind $view }
-	    update
-
-	    set filename $mopsi_options(ifilename)
-	    importMops $filename
-
-	    if { $ay_error < 2 } {
-		ayError 4 "importMops" "Done importing Mops scene from:"
-		ayError 4 "importMops" "$filename"
-	    } else {
-		ayError 2 "importMops"\
-		 "There were errors while importing the Mops scene from:"
-		ayError 2 "importMops" "$filename"
-	    }
-
-	    goTop
-	    selOb
-	    set ay(CurrentLevel) "root"
-	    set ay(SelectedLevel) "root"
-	    update
-
-	    uS
-	    rV
-	    set ay(sc) 1
-
-	    destroy .mopI
-	    foreach view $ay(views) { viewBind $view }
-	    update
-
-	    after idle viewMouseToCurrent
+	if { $ay_error < 2 } {
+	    ayError 4 "importMops" "Done importing the Mops scene from:"
+	    ayError 4 "importMops" "$filename"
+	} else {
+	    ayError 2 "importMops"\
+		"There were errors while importing the Mops scene from:"
+	    ayError 2 "importMops" "$filename"
 	}
 
-	button $f.bca -text "Cancel" -width 5 -command "\
+	goTop
+	selOb
+	set ay(CurrentLevel) "root"
+	set ay(SelectedLevel) "root"
+	update
+
+	uS
+	rV
+	set ay(sc) 1
+
+	destroy .mopI
+	foreach view $ay(views) { viewBind $view }
+	update
+
+	set mopsi_options(filename) $filename
+
+	after idle viewMouseToCurrent
+    }
+    # button
+
+    button $f.bca -text "Cancel" -width 5 -command "\
 		focus .;\
 		destroy .mopI"
 
-	pack $f.bok $f.bca -in $f -side left -fill x -expand yes
-	pack $f -in $w -side bottom -fill x
+    pack $f.bok $f.bca -in $f -side left -fill x -expand yes
+    pack $f -in $w -side bottom -fill x
 
-	winCenter $w
+    winCenter $w
 
-	focus $w.f2.bok
-
-    }
+    focus $w.f2.bok
 
  return;
 }
