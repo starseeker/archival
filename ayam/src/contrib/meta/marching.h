8e36529d9fdec5b307f7393ad0e70ad73f05cc6b
Randolf Schultz <randolf.schultz@gmail.com>
2006-01-01 08:37:24 +0000

    fixes for compilation as MacOSX bundle

--- ayam/src/contrib/meta/marching.h
+++ ayam/src/contrib/meta/marching.h
@@ -1,7 +1,7 @@
 /*
  * Ayam, a free 3D modeler for the RenderMan interface.
  *
- * Ayam is copyrighted 1998-2001 by Randolf Schultz
+ * Ayam is copyrighted 1998-2005 by Randolf Schultz
  * (rschultz@informatik.uni-rostock.de) and others.
  *
  * All rights reserved.
@@ -18,7 +18,7 @@
 #ifndef MARCHING_H
 #define MARCHING_H 1
 
-int edgeTable[256] = {
+static int edgeTable[256] = {
   0x0, 0x109, 0x203, 0x30a, 0x406, 0x50f, 0x605, 0x70c,
   0x80c, 0x905, 0xa0f, 0xb06, 0xc0a, 0xd03, 0xe09, 0xf00,
   0x190, 0x99, 0x393, 0x29a, 0x596, 0x49f, 0x795, 0x69c,
@@ -52,7 +52,7 @@ int edgeTable[256] = {
   0xf00, 0xe09, 0xd03, 0xc0a, 0xb06, 0xa0f, 0x905, 0x80c,
   0x70c, 0x605, 0x50f, 0x406, 0x30a, 0x203, 0x109, 0x0
 };
-int triTable[256][16] =
+static int triTable[256][16] =
   { {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
 {0, 8, 3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
 {0, 1, 9, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
