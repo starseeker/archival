34eda9c84c6d881813ccc6d394ed92d2a1200f7c
Randolf Schultz <randolf.schultz@gmail.com>
2006-02-12 03:05:11 +0000

    added align bevel feature

--- ayam/src/nurbs.h
+++ ayam/src/nurbs.h
@@ -389,7 +389,7 @@ int ay_npt_extrude(double height, ay_object *o,
 int ay_npt_gettangentfromcontrol(int closed, int n, int p,
 				 int stride, double *P, int a, double *t);
 
-int ay_npt_bevel(int type, double radius, ay_object *o,
+int ay_npt_bevel(int type, double radius, int align, ay_object *o,
 		 ay_nurbpatch_object **patch);
 
 int ay_npt_createcap(double z, ay_nurbcurve_object *curve,
