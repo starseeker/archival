022a338df52e5141822a4cfaf8b8f5cbc5bb2104
Randolf Schultz <randolf.schultz@gmail.com>
2006-11-30 20:59:05 +0000

    added help on object to context menu

--- ayam/src/tcl/tree.tcl
+++ ayam/src/tcl/tree.tcl
@@ -850,6 +850,9 @@ $m add command -label "Paste Object" -command "$em invoke 2"
 #$m add command -label "Paste (Move)" -command "cmovOb;uS;rV"
 $m add separator
 $m add command -label "Delete Object" -command "$em invoke 3"
+set hm $ay(helpmenu)
+$m add separator
+$m add command -label "Help on Object" -command "$hm invoke 1"
 
 if { $ay(ws) == "Aqua" && $ayprefs(SwapMB) } {
     bind $ay(tree) <ButtonPress-2> "tree_openPopup $ay(tree)"
