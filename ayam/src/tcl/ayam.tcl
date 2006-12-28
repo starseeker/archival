22407d9a70f330a8ca6704e0d0415ac8ba85c510
Randolf Schultz <randolf.schultz@gmail.com>
2006-12-28 21:01:28 +0000

    fix alt-rotation on Aqua

--- ayam/src/tcl/ayam.tcl
+++ ayam/src/tcl/ayam.tcl
@@ -677,8 +677,8 @@ if { $tcl_platform(platform) == "windows" } {
 	set ayprefs(FixX11Menu) 0
 
 	# on Mac OS X Aqua we have to use this for the Alt key
+	set ayviewshortcuts(RotMod) "Option"
 	if { $tcl_patchLevel < "8.4.14" } {
-	    set ayviewshortcuts(RotMod) "Option"
 	    set ayviewshortcuts(RotModKey) "Meta_L"
 	}
 	set ayviewshortcuts(Stretch) "Option-s"
