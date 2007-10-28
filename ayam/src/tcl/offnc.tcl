1466ab41d26815629f24f9d2b479a5b94f837961
Randolf Schultz <randolf.schultz@gmail.com>
2007-10-28 17:25:00 +0000

    initial revision

---
+++ ayam/src/tcl/offnc.tcl
@@ -0,0 +1,35 @@
+# Ayam, a free 3D modeler for the RenderMan interface.
+#
+# Ayam is copyrighted 1998-2007 by Randolf Schultz
+# (rschultz@informatik.uni-rostock.de) and others.
+#
+# All rights reserved.
+#
+# See the file License for details.
+
+# offnc.tcl - offset curves objects Tcl code
+
+set OffsetNC_props { Transformations Attributes Tags OffsetNCAttr }
+
+array set OffsetNCAttr {
+arr   OffsetNCAttrData
+sproc ""
+gproc ""
+w     fOffsetNCAttr
+}
+
+array set OffsetNCAttrData {
+DisplayMode 0
+NCInfoBall "N/A"
+}
+# create UI
+set w [frame $ay(pca).$OffsetNCAttr(w)]
+
+addCheck $w OffsetNCAttrData Revert
+addParam $w OffsetNCAttrData Offset
+
+addParam $w OffsetNCAttrData Tolerance
+addMenu $w OffsetNCAttrData DisplayMode $ay(ncdisplaymodes)
+
+addText $w OffsetNCAttrData "Created NURBS Curve:"
+addInfo $w OffsetNCAttrData NCInfo
