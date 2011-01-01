3675619ade1996859ad8dffe891165ff6274adf9
Randolf Schultz <randolf.schultz@gmail.com>
2011-01-01 19:40:44 +0000

    imp includes

--- ayam/src/plugins/subdivide/include/compat.h
+++ ayam/src/plugins/subdivide/include/compat.h
@@ -23,28 +23,19 @@ Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 
 
 #include <assert.h>
+#include <stdlib.h>
 
-// Windows-specific include for OpenGL and GLUT
-#ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#undef WIN32_LEAN_AND_MEAN
-#pragma warning(disable:4305)
-#pragma warning(disable:4244)
+#include <math.h>
+#ifndef M_PI
+#define M_PI		3.14159265358979323846
 #endif
 
 #include <iostream>
 #include <fstream>
 
 
-
 namespace std {}
 using namespace std;
-
-#include <math.h>
-#ifndef M_PI
-#define M_PI		3.14159265358979323846
-#endif
 
 
 inline void die() { 
