da69a3f0ea2c9c8b8646866697231ff290272251
Randolf Schultz <randolf.schultz@gmail.com>
2014-01-07 19:39:50 +0000

    extend copyright

--- ayam/src/tcl/about.tcl
+++ ayam/src/tcl/about.tcl
@@ -54,7 +54,7 @@ $w.ftext.text insert end \
 Ayam, a free 3D Tcl/Tk/OpenGL/RenderMan modeler
 
 Ayam is
-Copyright (c) 1998-2010 by Randolf Schultz
+Copyright (c) 1998-2014 by Randolf Schultz
 (randolf.schultz@gmail.com)
 All Rights Reserved
 
