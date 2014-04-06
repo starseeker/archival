6943a9361c097071d51c17e68ca6ca004c37fffe
Randolf Schultz <randolf.schultz@gmail.com>
2014-04-06 21:05:22 +0000

    code cleanup

--- ayam/src/nurbs/pmt.c
+++ ayam/src/nurbs/pmt.c
@@ -781,7 +781,7 @@ ay_pmt_tobezier(ay_pamesh_object *pm)
  double *n1, *n2, *n3, *n4;
  double m[16], tm[16], mu[16], mv[16], mut[16];
  double mb[16] = {1, 0, 0, 0,  -3, 3, 0, 0,  3, -6, 3, 0,  -1, 3, -3, 1};
- double mbi[16];
+ double mbi[16] = {0};
  double mh[16] = {1, -1, 0, 0,  -2, 3, 0, 0,  1, -2, 1, 0,  2, -3, 0, 1};
  double mc[16] = {0.5, -0.5, 0, 0,  -1.5, 2, 0.5, 0,  1.5, -2.5, 0, -1,
 		  -0.5, 1, -0.5, 0};
