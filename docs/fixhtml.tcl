fa7e30ee8f998a4985676d295bc532bda8e3e0b8
Randolf Schultz <randolf.schultz@gmail.com>
2014-05-23 09:54:56 +0000

    add overview

--- docs/fixhtml.tcl
+++ docs/fixhtml.tcl
@@ -4,6 +4,7 @@
 
 
 set procs { fixfigureb fixfiguree fixcaption }
+set procs {}
 
 # center all figures
 proc fixfigureb { buf outfile } {
@@ -69,5 +70,33 @@ foreach infilename [glob html/*.html] {
     file delete -force ${infilename}.tmp
 }
 # foreach
+
+# insert "overview" section into the TOC
+set infile [ open ayam.html r ]
+set outfile [ open ${ayam.html}.tmp w ]
+set found 0
+while { ![eof $infile] } {
+    gets $infile buf
+    puts $outfile $buf
+    if { $found == 0 } {
+	if { [string first "</EM>" $buf] > -1 }
+	{
+	    set overfile [ open ../overview.html r ]
+	    while { ![eof $overfile] } {
+		gets $overfile buf
+		puts $outfile $buf
+	    }
+	    close $overfile
+	    set found 1
+	    # eat <HR>
+	    gets $infile buf
+	}
+    }
+}
+close $infile
+close $outfile
+
+file copy -force ${infilename}.tmp ${infilename}
+file delete -force ${infilename}.tmp
 
 return;
