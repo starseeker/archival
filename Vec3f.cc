3f4c634775c628d2b73ebe78ec55dcdd8c373e58
Cliff Yapp <cliffyapp@gmail.com>
2014-07-03 23:44:32 -0400

    Update copyright and license statement per http://www-graphics.stanford.edu/software/volfill/ and http://graphics.stanford.edu/software/bsd-license.html

--- Vec3f.cc
+++ Vec3f.cc
@@ -1,23 +1,29 @@
 /*
-
-Brian Curless
-
-Computer Graphics Laboratory
-Stanford University
-
----------------------------------------------------------------------
-
-Copyright (1997) The Board of Trustees of the Leland Stanford Junior
-University. Except for commercial resale, lease, license or other
-commercial transactions, permission is hereby given to use, copy,
-modify this software for academic purposes only.  No part of this
-software or any derivatives thereof may be used in the production of
-computer models for resale or for use in a commercial
-product. STANFORD MAKES NO REPRESENTATIONS OR WARRANTIES OF ANY KIND
-CONCERNING THIS SOFTWARE.  No support is implied or provided.
-
-*/
-
+ * Copyright (c) 1995-2014, Stanford University
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Stanford University nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY STANFORD UNIVERSITY ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL STANFORD UNIVERSITY BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include "Vec3f.h"
 
