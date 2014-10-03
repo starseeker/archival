16e48715494f2074fe66a1ab110237d2f0039869
Randolf Schultz <randolf.schultz@gmail.com>
2014-10-03 11:02:29 +0000

    imp comments; code cleanup

--- ayam/src/aycore/pact.c
+++ ayam/src/aycore/pact.c
@@ -276,22 +276,22 @@ ay_pact_seltcb(struct Togl *togl, int argc, char *argv[])
 	  ay_viewt_winrecttoobj(togl, o, winX, winY, winX2, winY2, obj);
 
 	  /* create plane equation coefficients */
-	  ay_trafo_pointstoplane(obj[0], obj[1], obj[2],
+	  ay_geom_pointstoplane(obj[0], obj[1], obj[2],
 				 obj[3], obj[4], obj[5],
 				 obj[12], obj[13], obj[14],
 				 &(pl[0]), &(pl[1]), &(pl[2]), &(pl[3]));
 
-	  ay_trafo_pointstoplane(obj[3], obj[4], obj[5],
+	  ay_geom_pointstoplane(obj[3], obj[4], obj[5],
 				 obj[9], obj[10], obj[11],
 				 obj[15], obj[16], obj[17],
 				 &(pl[4]), &(pl[5]), &(pl[6]), &(pl[7]));
 
-	  ay_trafo_pointstoplane(obj[6], obj[7], obj[8],
+	  ay_geom_pointstoplane(obj[6], obj[7], obj[8],
 				 obj[18], obj[19], obj[20],
 				 obj[9], obj[10], obj[11],
 				 &(pl[8]), &(pl[9]), &(pl[10]), &(pl[11]));
 
-	  ay_trafo_pointstoplane(obj[0], obj[1], obj[2],
+	  ay_geom_pointstoplane(obj[0], obj[1], obj[2],
 				 obj[12], obj[13], obj[14],
 				 obj[6], obj[7], obj[8],
 				 &(pl[12]), &(pl[13]), &(pl[14]), &(pl[15]));
--- ayam/src/aycore/trafo.c
+++ ayam/src/aycore/trafo.c
@@ -1812,26 +1812,6 @@ ay_trafo_rotatematrix(double angle, double x, double y, double z, double *m)
 } /* ay_trafo_rotatematrix */
 
 
-/* ay_trafo_pointstoplane:
- *  convert 3 points to plane equation coefficients
- */
-void
-ay_trafo_pointstoplane(double x1, double y1, double z1,
-		       double x2, double y2, double z2,
-		       double x3, double y3, double z3,
-		       double *A, double *B, double *C, double *D)
-{
-
-  *A = y1 * (z2 - z3) + y2 * (z3 - z1) + y3 * (z1 - z2);
-  *B = z1 * (x2 - x3) + z2 * (x3 - x1) + z3 * (x1 - x2);
-  *C = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
-  *D = -(x1 * (y2 * z3 - y3 * z2) + x2 * (y3 * z1 - y1 * z3) +
-	 x3 * (y1 * z2 - y2 * z1));
-
- return;
-} /* ay_trafo_pointstoplane */
-
-
 /* ay_trafo_scalecog:
  *
  */
