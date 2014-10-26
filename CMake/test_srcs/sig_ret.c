282450cedc9e55f68f5258c4c92a4353049d6749
Cliff Yapp <cliffyapp@gmail.com>
2014-10-26 02:27:55 -0400

    Add a test for signal return types, add headers to the toplevel build, enable compilation of the first two lib compilations (datetime and gis).

---
+++ CMake/test_srcs/sig_ret.c
@@ -0,0 +1,14 @@
+#include <sys/types.h>
+#include <signal.h>
+#ifdef signal
+#undef signal
+#endif
+#ifdef __cplusplus
+extern "C" void (*signal (int, void (*)(int)))(int);
+#else
+void (*signal ()) ();
+#endif
+
+int main() {
+int i;
+; return 0; }
